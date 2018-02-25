80b4bb296862ffc8b8fc78169e7d3c1fdbc7ec86
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-02-25 22:14:07 +0000

    wip: rename the mainctivity xml to login activity

--- app/src/main/java/com/chesire/malime/LoginActivity.kt
+++ app/src/main/java/com/chesire/malime/LoginActivity.kt
@@ -17,7 +17,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         findViewById<Button>(R.id.login_button).setOnClickListener { executeLoginMethod() }
     }
