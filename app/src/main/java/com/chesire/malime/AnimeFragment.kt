d39cff7760fa96cb57c7c8b4eca932ec464b640b
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-03-10 18:07:58 +0000

    wip: change fragment to use the non-nullable inflater

--- app/src/main/java/com/chesire/malime/AnimeFragment.kt
+++ app/src/main/java/com/chesire/malime/AnimeFragment.kt
@@ -7,8 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 
 class AnimeFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_anime, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_anime, container, false)
     }
 
     companion object {
--- app/src/main/java/com/chesire/malime/MangaFragment.kt
+++ app/src/main/java/com/chesire/malime/MangaFragment.kt
@@ -7,8 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 
 class MangaFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_manga, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_manga, container, false)
     }
 
     companion object {
