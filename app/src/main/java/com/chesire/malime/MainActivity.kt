5071c8415dfd8ed33f17f60bfd67cf891d30f98d
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-03-10 18:13:30 +0000

    wip: add the options menu item to go to the users profile

--- app/src/main/java/com/chesire/malime/MainActivity.kt
+++ app/src/main/java/com/chesire/malime/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.chesire.malime
 
+import android.net.Uri
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
@@ -29,5 +32,24 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.activity_main_frame, AnimeFragment.newInstance())
                 .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.menu_options_view_profile) {
+            CustomTabsIntent.Builder()
+                    .build()
+                    .launchUrl(this, Uri.parse("https://myanimelist.net/"))
+            // This should go to the users profile
+            // https://myanimelist.net/profile/$NAME
+            // But need to store the credentials/username first
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
