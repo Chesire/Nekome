165923bebc3f904fc58d027df88f421d2a2e35f6
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-03-11 17:10:07 +0000

    wip: add basic methods to call the new apis

--- app/src/main/java/com/chesire/malime/mal/MalManager.kt
+++ app/src/main/java/com/chesire/malime/mal/MalManager.kt
@@ -1,6 +1,8 @@
 package com.chesire.malime.mal
 
+import com.chesire.malime.models.Anime
 import com.chesire.malime.models.Entry
+import com.chesire.malime.models.Manga
 import io.reactivex.Observable
 import timber.log.Timber
 
@@ -8,6 +10,34 @@ class MalManager(
         auth: String,
         private val api: MalApi = MalApi(auth)
 ) {
+    fun getAllAnime(username: String): Observable<List<Anime>> {
+        return Observable.create { subscriber ->
+            val callResponse = api.getAllAnime(username)
+            val response = callResponse.execute()
+
+            if (response.isSuccessful) {
+                Timber.i("Get all anime successful")
+            } else {
+                Timber.e(Throwable(response.message()))
+                subscriber.onError(Throwable(response.message()))
+            }
+        }
+    }
+
+    fun getAllManga(username: String): Observable<List<Manga>> {
+        return Observable.create { subscriber ->
+            val callResponse = api.getAllManga(username)
+            val response = callResponse.execute()
+
+            if (response.isSuccessful) {
+                Timber.i("Get all manga successful")
+            } else {
+                Timber.e(Throwable(response.message()))
+                subscriber.onError(Throwable(response.message()))
+            }
+        }
+    }
+
     fun loginToAccount(): Observable<Any> {
         return Observable.create { subscriber ->
             val callResponse = api.loginToAccount()
