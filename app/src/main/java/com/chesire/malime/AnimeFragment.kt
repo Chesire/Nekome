dbfecfa72b60862af4e9db6d045a4ba5e084f8e3
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-03-11 19:15:59 +0000

    wip: add pull to refresh layout to refresh anime list

--- app/src/main/java/com/chesire/malime/AnimeFragment.kt
+++ app/src/main/java/com/chesire/malime/AnimeFragment.kt
@@ -2,13 +2,62 @@ package com.chesire.malime
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.chesire.malime.mal.MalManager
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class AnimeFragment : Fragment() {
+    private var disposables = CompositeDisposable()
+    private lateinit var username: String
+    private lateinit var malManager: MalManager
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val sharedPref = SharedPref(context!!)
+        username = sharedPref.getUsername()
+        malManager = MalManager(sharedPref.getAuth())
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_anime, container, false)
+        val view = inflater.inflate(R.layout.fragment_anime, container, false)
+
+        swipeRefreshLayout = view.findViewById(R.id.anime_swipe_refresh)
+        swipeRefreshLayout.setOnRefreshListener {
+            executeLoadAnime()
+        }
+
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        disposables = CompositeDisposable()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposables.clear()
+    }
+
+    private fun executeLoadAnime() {
+        disposables.add(malManager.getAllAnime(username)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { _ ->
+                            swipeRefreshLayout.isRefreshing = false
+                        },
+                        { _ ->
+                            swipeRefreshLayout.isRefreshing = false
+                        }
+                ))
     }
 
     companion object {
