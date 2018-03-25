4eafe5c81fd03029bf22e2450751ae4cf7c91348
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-03-25 17:54:39 +0100

    wip: add the singleton holder to make sure synchronization of the roomdb is not an issue

--- app/src/main/java/com/chesire/malime/room/MalimeDatabase.kt
+++ app/src/main/java/com/chesire/malime/room/MalimeDatabase.kt
@@ -5,30 +5,16 @@ import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
 import com.chesire.malime.models.Anime
+import com.chesire.malime.util.SingletonHolder
 
 @Database(entities = [(Anime::class)], version = 1)
 abstract class MalimeDatabase : RoomDatabase() {
     abstract fun animeDao(): AnimeDao
 
-    companion object {
-        private const val dbName: String = "malime.db"
-        private var INSTANCE: MalimeDatabase? = null
-
-        fun getInstance(context: Context): MalimeDatabase? {
-            synchronized(MalimeDatabase::class) {
-                if (INSTANCE == null) {
-                    INSTANCE = Room.databaseBuilder(
-                        context.applicationContext,
-                        MalimeDatabase::class.java,
-                        dbName
-                    ).build()
-                }
-            }
-            return INSTANCE
-        }
-
-        fun destroyInstance() {
-            INSTANCE = null
-        }
-    }
+    companion object : SingletonHolder<MalimeDatabase, Context>({
+        Room.databaseBuilder(
+            it.applicationContext,
+            MalimeDatabase::class.java, "malime.db"
+        ).build()
+    })
 }
