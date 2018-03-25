8b43aa5aae87659c8fac76e0d766a2931323fc75
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-03-26 00:41:13 +0100

    wip: make a jobscheduler to update the internal data periodically, debug code has been left in to notify

---
+++ app/src/main/java/com/chesire/malime/util/PeriodicUpdateService.kt
@@ -0,0 +1,68 @@
+package com.chesire.malime.util
+
+import android.app.NotificationManager
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.support.v4.app.NotificationCompat
+import com.chesire.malime.R
+import com.chesire.malime.mal.MalManager
+import com.chesire.malime.models.Anime
+import com.chesire.malime.room.MalimeDatabase
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+import java.util.Calendar
+
+class PeriodicUpdateService : JobService() {
+    override fun onStopJob(params: JobParameters?): Boolean {
+        return false
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        getLatestAnime(params)
+        return true
+    }
+
+    private fun getLatestAnime(params: JobParameters?) {
+        val sharedPref = SharedPref(applicationContext)
+        val malManager = MalManager(sharedPref.getAuth())
+
+        malManager.getAllAnime(sharedPref.getUsername())
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result ->
+                    Timber.d("Successfully got latest anime from MAL")
+                    executeSaveToLocalDb(params, result.second)
+                },
+                { _ ->
+                    Timber.e("Failed to get latest anime from MAL")
+                    jobFinished(params, true)
+                }
+            )
+    }
+
+    private fun executeSaveToLocalDb(params: JobParameters?, animes: List<Anime>) {
+        Timber.d("Updating local DB for all anime")
+
+        // Looks like this doesn't have to be disposed off
+        Completable
+            .fromAction({
+                MalimeDatabase.getInstance(applicationContext).animeDao().insertAll(animes)
+                jobFinished(params, false)
+
+                val b = NotificationCompat.Builder(applicationContext, "MALIMETEST")
+                b.setContentTitle("Pulled Data")
+                b.setContentText("New MALIME Data at ${Calendar.getInstance().time}")
+                b.setSmallIcon(R.drawable.ic_filter_list_black)
+
+                val man =
+                    applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                man.notify(1, b.build())
+            })
+            .subscribeOn(Schedulers.io())
+            .subscribe()
+    }
+}
