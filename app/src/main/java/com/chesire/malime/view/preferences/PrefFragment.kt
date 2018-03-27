e3d363abdef311414a054e52beb2cde2c9d8716c
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-03-27 21:05:06 +0100

    fix(sharedpref): allow reading of the sharedprefs due to not using the correct file

--- app/src/main/java/com/chesire/malime/view/preferences/PrefFragment.kt
+++ app/src/main/java/com/chesire/malime/view/preferences/PrefFragment.kt
@@ -3,9 +3,13 @@ package com.chesire.malime.view.preferences
 import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
 import com.chesire.malime.R
+import com.chesire.malime.util.SharedPref
 
 class PrefFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        val manager = preferenceManager
+        manager.sharedPreferencesName = SharedPref(context!!).sharedPrefFile
         addPreferencesFromResource(R.xml.preferences)
     }
 
