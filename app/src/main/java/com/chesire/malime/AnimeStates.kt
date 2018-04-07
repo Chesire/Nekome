b01fc24edff12f2cdfa13e889c2a376928aa6ac0
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-04-07 12:42:08 +0100

    wip: add some docs to the animestates

--- app/src/main/java/com/chesire/malime/AnimeStates.kt
+++ app/src/main/java/com/chesire/malime/AnimeStates.kt
@@ -1,7 +1,24 @@
 package com.chesire.malime
 
+import com.chesire.malime.models.UpdateAnime
+
+/*
+    Id is the value that is stored up on MAL,
+    but due to displaying arrays and just using index it is easier to use the surface id
+ */
+
+/**
+ * Enum to store the possible states an anime series can be in,
+ * id will be the value that gets uploaded to [UpdateAnime.status] to update its status on MAL.
+ */
 enum class AnimeStates(
+    /**
+     * The state based on MAL.
+     */
     val id: Int,
+    /**
+     * The state based on an internal value.
+     */
     val surfaceId: Int
 ) {
     WATCHING(1, 0),
