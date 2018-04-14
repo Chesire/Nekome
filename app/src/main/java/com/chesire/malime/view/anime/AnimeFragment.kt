490dbbb9cb6eb8314c0fc733f5faed0780b644f4
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-04-14 10:19:07 +0100

    fix(lint): remove linting issues in the animefragment

--- app/src/main/java/com/chesire/malime/view/anime/AnimeFragment.kt
+++ app/src/main/java/com/chesire/malime/view/anime/AnimeFragment.kt
@@ -63,7 +63,9 @@ class AnimeFragment : Fragment(),
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_maldisplay, container, false)
 
