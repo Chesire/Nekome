28ceb7a70d125f8cf575853d6569074c132146fe
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-04-14 15:43:41 +0100

    wip: begin adding the methods to pull search data

--- app/src/main/java/com/chesire/malime/view/search/SearchFragment.kt
+++ app/src/main/java/com/chesire/malime/view/search/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.chesire.malime.view.search
 
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +9,30 @@ import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.RadioGroup
 import com.chesire.malime.R
+import com.chesire.malime.mal.MalManager
+import com.chesire.malime.util.SharedPref
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 
 class SearchFragment : Fragment() {
     private val nsfwAllowedKey = "nsfwAllowed"
     private val checkedOptionKey = "checkedOption"
+    private val searchTextKey = "searchText"
 
+    private var disposables = CompositeDisposable()
     private var nsfwAllowed = false
     private var checkedOption = 0
+
+    private lateinit var malManager: MalManager
+    private lateinit var searchText: TextInputEditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val sharedPref = SharedPref(context!!)
+        malManager = MalManager(sharedPref.getAuth())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,14 +44,16 @@ class SearchFragment : Fragment() {
         val searchRadioGroup = view.findViewById<RadioGroup>(R.id.search_option_choices)
         searchRadioGroup.setOnCheckedChangeListener { _, checkedId ->
             checkedOption = checkedId
-            searchOptionChanged()
+            executeSearch()
         }
 
         val nsfwCheckbox = view.findViewById<CheckBox>(R.id.search_option_nsfw_allowed)
         nsfwCheckbox.setOnClickListener {
             nsfwAllowed = (it as CheckBox).isChecked
-            searchOptionChanged()
+            executeSearch()
         }
+
+        searchText = view.findViewById(R.id.search_search_term_edit_text)
 
         if (savedInstanceState == null) {
             checkedOption = searchRadioGroup.checkedRadioButtonId
@@ -41,19 +61,55 @@ class SearchFragment : Fragment() {
         } else {
             checkedOption = savedInstanceState.getInt(checkedOptionKey)
             nsfwAllowed = savedInstanceState.getBoolean(nsfwAllowedKey)
+            searchText.setText(savedInstanceState.getString(searchTextKey))
         }
 
         return view
     }
 
+    override fun onResume() {
+        super.onResume()
+        disposables = CompositeDisposable()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposables.clear()
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putBoolean(nsfwAllowedKey, nsfwAllowed)
         outState.putInt(checkedOptionKey, checkedOption)
+        outState.putString(searchTextKey, searchText.text.toString())
         super.onSaveInstanceState(outState)
     }
 
-    private fun searchOptionChanged() {
-        // perform search again
+    private fun executeSearch() {
+        if (searchText.text.isNullOrBlank()) {
+            Timber.w("No text entered to search for")
+            return
+        }
+
+        val searchMethod = when (checkedOption) {
+            R.id.search_option_anime_choice -> malManager.searchForAnime(searchText.text.toString())
+            R.id.search_option_manga_choice -> malManager.searchForAnime(searchText.text.toString())
+            else -> {
+                Timber.e("Unknown search method selected")
+                return
+            }
+        }
+
+        disposables.add(searchMethod
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+
+                },
+                {
+
+                }
+            ))
     }
 
     companion object {
