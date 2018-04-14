13066e23cfa4202a87fa1a97b8365209a4e0ee44
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-04-14 11:44:00 +0100

    wip: hook the ui up to the fragment

--- app/src/main/java/com/chesire/malime/view/search/SearchFragment.kt
+++ app/src/main/java/com/chesire/malime/view/search/SearchFragment.kt
@@ -5,15 +5,55 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.RadioGroup
 import com.chesire.malime.R
 
 class SearchFragment : Fragment() {
+    private val nsfwAllowedKey = "nsfwAllowed"
+    private val checkedOptionKey = "checkedOption"
+
+    private var nsfwAllowed = false
+    private var checkedOption = 0
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        val searchRadioGroup = view.findViewById<RadioGroup>(R.id.search_option_choices)
+        searchRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            checkedOption = checkedId
+            searchOptionChanged()
+        }
+
+        val nsfwCheckbox = view.findViewById<CheckBox>(R.id.search_option_nsfw_allowed)
+        nsfwCheckbox.setOnClickListener {
+            nsfwAllowed = (it as CheckBox).isChecked
+            searchOptionChanged()
+        }
+
+        if (savedInstanceState == null) {
+            checkedOption = searchRadioGroup.checkedRadioButtonId
+            nsfwAllowed = nsfwCheckbox.isChecked
+        } else {
+            checkedOption = savedInstanceState.getInt(checkedOptionKey)
+            nsfwAllowed = savedInstanceState.getBoolean(nsfwAllowedKey)
+        }
+
+        return view
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(nsfwAllowedKey, nsfwAllowed)
+        outState.putInt(checkedOptionKey, checkedOption)
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun searchOptionChanged() {
+        // perform search again
     }
 
     companion object {
