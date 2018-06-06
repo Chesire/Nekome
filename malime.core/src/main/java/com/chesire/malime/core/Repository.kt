fa0d521f1de16d1100d3d6f15a0eaeb732f0cdb3
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-06-07 00:29:32 +0100

    wip: add interface for the repository

---
+++ malime.core/src/main/java/com/chesire/malime/core/Repository.kt
@@ -0,0 +1,11 @@
+package com.chesire.malime.core
+
+import com.chesire.malime.core.models.MalimeModel
+import io.reactivex.Observable
+import io.reactivex.Single
+
+interface Repository {
+    fun observeLibrary(): Observable<List<MalimeModel>>
+    fun updateLibraryFromApi(): Single<Boolean>
+    fun clearLocalLibrary()
+}
