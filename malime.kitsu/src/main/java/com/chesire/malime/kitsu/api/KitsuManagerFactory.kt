60085b2a7d0de9e1ff9b30b7a5af638d80b1d7b9
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-06-09 15:26:37 +0100

    wip: make the kitsu factory store its manager based on userid

--- malime.kitsu/src/main/java/com/chesire/malime/kitsu/api/KitsuManagerFactory.kt
+++ malime.kitsu/src/main/java/com/chesire/malime/kitsu/api/KitsuManagerFactory.kt
@@ -1,10 +1,21 @@
 package com.chesire.malime.kitsu.api
 
 class KitsuManagerFactory {
+    private val managers = HashMap<Int, KitsuManager>()
+
     fun get(accessToken: String = "", userId: Int = 0): KitsuManager {
-        return KitsuManager(
-            KitsuApi(accessToken),
-            userId
-        )
+        if (!managers.containsKey(userId)) {
+            // remove the manager at user id 0, as its the default
+            if (userId == 0) {
+                managers.remove(userId)
+            }
+
+            managers[userId] = KitsuManager(
+                KitsuApi(accessToken),
+                userId
+            )
+        }
+
+        return managers[userId]!!
     }
 }
