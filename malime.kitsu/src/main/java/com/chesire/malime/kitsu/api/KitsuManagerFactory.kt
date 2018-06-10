5f4d205a073fbb693ceab8b97bfdfcb4d0b62cb9
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-06-10 18:22:03 +0100

    wip: remove the manager at the correct time in the factory

--- malime.kitsu/src/main/java/com/chesire/malime/kitsu/api/KitsuManagerFactory.kt
+++ malime.kitsu/src/main/java/com/chesire/malime/kitsu/api/KitsuManagerFactory.kt
@@ -4,12 +4,13 @@ class KitsuManagerFactory {
     private val managers = HashMap<Int, KitsuManager>()
 
     fun get(accessToken: String = "", userId: Int = 0): KitsuManager {
-        if (!managers.containsKey(userId)) {
-            // remove the manager at user id 0, as its the default
-            if (userId == 0) {
-                managers.remove(userId)
-            }
+        // remove the manager at user id 0, as its the default used to login
+        // sorry person with userId of 0...
+        if (userId == 0) {
+            managers.remove(userId)
+        }
 
+        if (!managers.containsKey(userId)) {
             managers[userId] = KitsuManager(
                 KitsuApi(accessToken),
                 userId
