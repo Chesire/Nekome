1f14ea0e1d8d5eaffc81b8360ae3130b928ff256
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-06-13 21:09:59 +0100

    wip: remove the old search items, as they are no longer needed

--- app/src/main/java/com/chesire/malime/view/search/SearchInteractionListener.kt
+++
@@ -1,8 +0,0 @@
-package com.chesire.malime.view.search
-
-import com.chesire.malime.mal.models.Entry
-
-interface SearchInteractionListener {
-    fun onAddPressed(selectedEntry: Entry, callback: (Boolean) -> Unit)
-    fun onImageClicked(entry: Entry)
-}
