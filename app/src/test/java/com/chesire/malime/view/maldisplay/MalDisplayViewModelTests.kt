dfda9f657e5b369f8ac28b9a52441a67df1439fb
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-06-22 21:50:29 +0100

    test(maldisplay): add unit tests for the maldisplayviewmodel

--- app/src/test/java/com/chesire/malime/view/maldisplay/MalDisplayViewModelTests.kt
+++ app/src/test/java/com/chesire/malime/view/maldisplay/MalDisplayViewModelTests.kt
@@ -3,15 +3,22 @@ package com.chesire.malime.view.maldisplay
 import android.arch.core.executor.testing.InstantTaskExecutorRule
 import android.arch.lifecycle.Observer
 import android.test.mock.MockApplication
+import com.chesire.malime.core.flags.UserSeriesStatus
 import com.chesire.malime.core.models.MalimeModel
 import com.chesire.malime.core.repositories.Library
 import com.chesire.malime.customMock
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.schedulers.TestScheduler
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
 
 @Suppress("DEPRECATION")
 class MalDisplayViewModelTests {
@@ -26,6 +33,11 @@ class MalDisplayViewModelTests {
 
     @Before
     fun setup() {
+        val mockItems: Observable<List<MalimeModel>> = customMock()
+        val flowableItems: Flowable<List<MalimeModel>> = customMock()
+        `when`(mockItems.toFlowable(BackpressureStrategy.ERROR)).thenReturn(flowableItems)
+        `when`(library.observeLibrary()).thenReturn(mockItems)
+
         testObject = MalDisplayViewModel(
             MockApplication(),
             library,
@@ -44,31 +56,105 @@ class MalDisplayViewModelTests {
 
     @Test
     fun `check for latest series progresses to error state on failure`() {
+        `when`(
+            library.updateLibraryFromApi()
+        ).thenReturn(
+            Observable.error(Exception("Test Exception"))
+        )
 
+        testObject.checkForLatestSeries()
+        testScheduler.triggerActions()
+
+        verify(updateObserver).onChanged(UpdatingSeriesStatus.Updating)
+        verify(updateObserver).onChanged(UpdatingSeriesStatus.Error)
     }
 
     @Test
     fun `check for latest series progresses to success state on success`() {
+        val newItems = listOf<MalimeModel>()
+        `when`(
+            library.updateLibraryFromApi()
+        ).thenReturn(
+            Observable.just(newItems)
+        )
 
+        testObject.checkForLatestSeries()
+        testScheduler.triggerActions()
+
+        verify(updateObserver).onChanged(UpdatingSeriesStatus.Updating)
+        verify(updateObserver).onChanged(UpdatingSeriesStatus.Finished)
     }
 
     @Test
     fun `check for latest series updates local library on success`() {
+        val newItems = listOf<MalimeModel>()
+        `when`(
+            library.updateLibraryFromApi()
+        ).thenReturn(
+            Observable.just(newItems)
+        )
 
+        testObject.checkForLatestSeries()
+        testScheduler.triggerActions()
+
+        verify(library).insertIntoLocalLibrary(newItems)
     }
 
     @Test
     fun `update series fires callback on failure`() {
+        val malimeModel: MalimeModel = customMock()
+        var callbackCalled = false
 
+        `when`(
+            library.sendUpdateToApi(malimeModel, 5, UserSeriesStatus.Completed)
+        ).thenReturn(
+            Single.error(Exception("Test Exception"))
+        )
+
+        testObject.updateSeries(malimeModel, 5, UserSeriesStatus.Completed) {
+            callbackCalled = false
+        }
+        testScheduler.triggerActions()
+
+        assert(!callbackCalled)
     }
 
     @Test
     fun `update series fires callback on success`() {
+        val malimeModel: MalimeModel = customMock()
+        val returnedModel: MalimeModel = customMock()
+        var callbackCalled = false
 
+        `when`(
+            library.sendUpdateToApi(malimeModel, 5, UserSeriesStatus.Completed)
+        ).thenReturn(
+            Single.just(returnedModel)
+        )
+
+        testObject.updateSeries(malimeModel, 5, UserSeriesStatus.Completed) {
+            callbackCalled = true
+        }
+        testScheduler.triggerActions()
+
+        assert(callbackCalled)
     }
 
     @Test
     fun `update series updates local library on success`() {
+        val malimeModel: MalimeModel = customMock()
+        val returnedModel: MalimeModel = customMock()
 
+        `when`(
+            library.sendUpdateToApi(malimeModel, 5, UserSeriesStatus.Completed)
+        ).thenReturn(
+            Single.just(returnedModel)
+        )
+
+        testObject.updateSeries(malimeModel, 5, UserSeriesStatus.Completed) {
+            // No callback checked here
+        }
+        testScheduler.triggerActions()
+
+        verify(library).updateInLocalLibrary(returnedModel)
     }
 }
