25b363c325bd5d3efa3d6c8ae765a5d3f3b3bede
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-06-25 20:50:12 +0100

    wip: add a new subtype to extend what each malimemodel is for

---
+++ malime.core/src/main/java/com/chesire/malime/core/flags/SubType.kt
@@ -0,0 +1,40 @@
+package com.chesire.malime.core.flags
+
+enum class SubType(
+    val internalId: Int,
+    val malId: Int,// this might be a string in MAL?
+    val kitsuString: String
+) {
+    Unknown(0, 0, "unknown"),
+
+    // :Anime
+    ONA(1, 1, "ONA"),
+    OVA(2, 2, "OVA"),
+    TV(3, 3, "TV"),
+    Movie(4, 4, "movie"),
+    Music(5, 5, "music"),
+    Special(6, 6, "special"),
+
+    // :Manga
+    Doujin(7, 7, "doujin"),
+    Manga(8, 8, "manga"),
+    Manhua(9, 9, "manhua"),
+    Manhwa(10, 10, "manhwa"),
+    Novel(11, 11, "novel"),
+    OEL(12, 12, "oel"),
+    OneShot(13, 13, "oneshot");
+
+    companion object {
+        fun getSubTypeForKitsuString(inputString: String): SubType {
+            return SubType.values().find { it.kitsuString == inputString } ?: Unknown
+        }
+
+        fun getSubTypeForMalId(id: Int): SubType {
+            return SubType.values().find { it.malId == id } ?: Unknown
+        }
+
+        fun getSubTypeForInternalId(id: Int): SubType {
+            return SubType.values().find { it.internalId == id } ?: Unknown
+        }
+    }
+}
