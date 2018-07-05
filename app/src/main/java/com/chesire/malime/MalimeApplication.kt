dce3d85b417daaebf7a28a4563f725bbad7e3776
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-07-05 22:01:15 +0100

    wip: add strictmode violation testing

--- app/src/main/java/com/chesire/malime/MalimeApplication.kt
+++ app/src/main/java/com/chesire/malime/MalimeApplication.kt
@@ -1,6 +1,7 @@
 package com.chesire.malime
 
 import android.app.Application
+import android.os.StrictMode
 import com.chesire.malime.util.SharedPref
 import com.crashlytics.android.Crashlytics
 import com.squareup.leakcanary.LeakCanary
@@ -8,16 +9,12 @@ import io.fabric.sdk.android.Fabric
 import io.reactivex.plugins.RxJavaPlugins
 import timber.log.Timber
 
+
 class MalimeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return
-        }
-        LeakCanary.install(this)
+        startLeakCanary()
 
         // This is to handle switching fragments quickly while a request is occurring.
         // TODO: Revisit this, as will require some more reading into RXJava
@@ -27,10 +24,38 @@ class MalimeApplication : Application() {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+            startStrictMode()
         } else {
             if (SharedPref(applicationContext).getAllowCrashReporting()) {
                 Fabric.with(this, Crashlytics())
             }
         }
+    }
+
+    private fun startLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
+    }
+
+    private fun startStrictMode() {
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .detectCustomSlowCalls()
+                .penaltyLog()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
     }
 }
