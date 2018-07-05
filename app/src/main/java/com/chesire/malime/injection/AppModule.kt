e06b0505bd5b16815df373d302b3bdd429ce294f
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-07-05 23:35:20 +0100

    wip: remove the singleton flag from the appmodule context, from what ive read it shouldnt be needed

--- app/src/main/java/com/chesire/malime/injection/AppModule.kt
+++ app/src/main/java/com/chesire/malime/injection/AppModule.kt
@@ -4,11 +4,9 @@ import android.app.Application
 import android.content.Context
 import dagger.Module
 import dagger.Provides
-import javax.inject.Singleton
 
 @Module
 internal class AppModule {
-    @Singleton
     @Provides
     fun provideApplicationContext(application: Application): Context {
         return application.applicationContext
