baa10a411fcaa7bbe5929690e764b1d68d7982a1
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-07-05 23:18:02 +0100

    wip: make the launchactivity use dagger injection

--- app/src/main/java/com/chesire/malime/LaunchActivity.kt
+++ app/src/main/java/com/chesire/malime/LaunchActivity.kt
@@ -6,13 +6,17 @@ import android.os.Bundle
 import com.chesire.malime.util.SharedPref
 import com.chesire.malime.view.MainActivity
 import com.chesire.malime.view.login.LoginActivity
+import dagger.android.AndroidInjection
+import javax.inject.Inject
 
 class LaunchActivity : Activity() {
+    @Inject
+    lateinit var sharedPref: SharedPref
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
 
-        val sharedPref = SharedPref(this)
         val loadIntent =
             if (sharedPref.getAuth().authToken.isNotBlank() &&
                 (sharedPref.getUsername().isNotEmpty() || sharedPref.getUserId() != 0)
