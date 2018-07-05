d4470746631201e5ee32ab36e352d603385900b7
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-07-05 23:58:05 +0100

    wip: update the viewmodelkey to match the google sample

--- app/src/main/java/com/chesire/malime/injection/ViewModelKey.kt
+++ app/src/main/java/com/chesire/malime/injection/ViewModelKey.kt
@@ -2,11 +2,14 @@ package com.chesire.malime.injection
 
 import android.arch.lifecycle.ViewModel
 import dagger.MapKey
-import java.lang.annotation.Documented
 import kotlin.reflect.KClass
 
-@Documented
-@Target(AnnotationTarget.FUNCTION)
+@MustBeDocumented
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
 @Retention(AnnotationRetention.RUNTIME)
 @MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
