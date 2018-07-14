ae6ff3c035ff7706b969b079ea26dfe6c8cb1f2e
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-07-14 14:55:19 +0100

    wip: add the new service to be dagger injected

--- app/src/main/java/com/chesire/malime/injection/androidmodules/ServiceModule.kt
+++ app/src/main/java/com/chesire/malime/injection/androidmodules/ServiceModule.kt
@@ -1,6 +1,7 @@
 package com.chesire.malime.injection.androidmodules
 
 import com.chesire.malime.service.PeriodicUpdateService
+import com.chesire.malime.service.RefreshTokenService
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -9,4 +10,7 @@ import dagger.android.ContributesAndroidInjector
 abstract class ServiceModule {
     @ContributesAndroidInjector
     internal abstract fun contributePeriodicUpdateService(): PeriodicUpdateService
+
+    @ContributesAndroidInjector
+    internal abstract fun contributeRefreshTokenService(): RefreshTokenService
 }
