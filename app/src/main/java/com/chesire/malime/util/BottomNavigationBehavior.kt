273d25e1a6cefc94403b496cc302082f7edb100d
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-08-08 23:14:14 +0100

    wip: add behavior class for the bottom navigation
    Add class to handle lowering the bottom navigation when it is scrolled. Code is based on: https://android.jlelse.eu/scroll-your-bottom-navigation-view-away-with-10-lines-of-code-346f1ed40e9e

---
+++ app/src/main/java/com/chesire/malime/util/BottomNavigationBehavior.kt
@@ -0,0 +1,62 @@
+package com.chesire.malime.util
+
+import android.content.Context
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.Snackbar
+import android.support.v4.view.ViewCompat
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * Provides behavior to make the bottom navigation scroll down when the frame above it scrolls
+ */
+@Suppress("UNUSED")
+class BottomNavigationBehavior<V : View>(
+    context: Context,
+    attrs: AttributeSet
+) : CoordinatorLayout.Behavior<V>(context, attrs) {
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun layoutDependsOn(parent: CoordinatorLayout?, child: V, dependency: View?): Boolean {
+        if (dependency is Snackbar.SnackbarLayout) {
+            updateSnackbar(child, dependency)
+        }
+        return super.layoutDependsOn(parent, child, dependency)
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+        child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+    }
+
+    private fun updateSnackbar(child: View, snackbarLayout: Snackbar.SnackbarLayout) {
+        if (snackbarLayout.layoutParams is CoordinatorLayout.LayoutParams) {
+            val params = snackbarLayout.layoutParams as CoordinatorLayout.LayoutParams
+
+            params.anchorId = child.id
+            params.anchorGravity = Gravity.TOP
+            params.gravity = Gravity.TOP
+            snackbarLayout.layoutParams = params
+        }
+    }
+}
