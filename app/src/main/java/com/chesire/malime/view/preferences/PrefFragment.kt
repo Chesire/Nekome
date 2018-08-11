c5e9fa82db6f85b80e14b25311ea68aef7bde4d9
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-08-11 12:26:36 +0100

    wip: add preference item to display licenses
    Add a field in the preferences pages, that when pressed will launch the license activity

--- app/src/main/java/com/chesire/malime/view/preferences/PrefFragment.kt
+++ app/src/main/java/com/chesire/malime/view/preferences/PrefFragment.kt
@@ -1,16 +1,26 @@
 package com.chesire.malime.view.preferences
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
 import com.chesire.malime.R
 import com.chesire.malime.util.SHARED_PREF_FILE
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class PrefFragment : PreferenceFragmentCompat() {
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         val manager = preferenceManager
         manager.sharedPreferencesName = SHARED_PREF_FILE
         addPreferencesFromResource(R.xml.preferences)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        findPreference(getString(R.string.key_view_licenses)).setOnPreferenceClickListener {
+            startActivity(Intent(requireActivity(), OssLicensesMenuActivity::class.java))
+            true
+        }
     }
 
     companion object {
