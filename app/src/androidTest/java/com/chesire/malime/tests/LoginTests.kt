21c128e0003432de88e00a21e3752a78be7d8133
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-08-12 21:28:15 +0100

    wip: add test for reaching the main activity

--- app/src/androidTest/java/com/chesire/malime/tests/LoginTests.kt
+++ app/src/androidTest/java/com/chesire/malime/tests/LoginTests.kt
@@ -4,8 +4,12 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.clearText
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.action.ViewActions.closeSoftKeyboard
+import android.support.test.espresso.action.ViewActions.pressImeActionButton
 import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.intent.Intents
+import android.support.test.espresso.intent.Intents.intended
+import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import android.support.test.espresso.matcher.RootMatchers.withDecorView
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
@@ -17,8 +21,10 @@ import com.chesire.malime.INVALID_USERNAME
 import com.chesire.malime.R
 import com.chesire.malime.VALID_PASSWORD
 import com.chesire.malime.VALID_USERNAME
+import com.chesire.malime.view.MainActivity
 import com.chesire.malime.view.login.LoginActivity
 import org.hamcrest.CoreMatchers.not
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -83,5 +89,39 @@ class LoginTests {
         onView(withText(R.string.login_failure))
             .inRoot(withDecorView(not(activityRule.activity.window.decorView)))
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun canNavigateFieldsWithIMEButton() {
+        onView(withId(R.id.login_username_edit_text)).perform(clearText(), pressImeActionButton())
+        onView(withId(R.id.login_password_edit_text)).perform(clearText(), pressImeActionButton())
+
+        onView(withText(R.string.login_failure_email))
+            .inRoot(withDecorView(not(activityRule.activity.window.decorView)))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun correctDetailsLaunchesMainActivity() {
+        Intents.init()
+
+        onView(withId(R.id.login_username_edit_text)).perform(
+            typeText(VALID_USERNAME),
+            pressImeActionButton()
+        )
+        onView(withId(R.id.login_password_edit_text)).perform(
+            typeText(VALID_PASSWORD),
+            pressImeActionButton()
+        )
+
+        intended(hasComponent(MainActivity::class.java.name))
+        Intents.release()
+    }
+
+    @Test
+    @Ignore
+    fun pressingTheCreateAccountLinkOpensPage() {
+        // Couldn't find anything that says how this could be tested...
+        // Leaving this here as it will need to be done at some point
     }
 }
