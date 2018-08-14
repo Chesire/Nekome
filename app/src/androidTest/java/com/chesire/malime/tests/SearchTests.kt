e82914e09703596853526477641782da83d82865
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-08-14 22:18:41 +0100

    wip: add matcher for toasts
    Toasts have an issue being caught by espresso, so use a new matcher to catch toasts especially

--- app/src/androidTest/java/com/chesire/malime/tests/SearchTests.kt
+++ app/src/androidTest/java/com/chesire/malime/tests/SearchTests.kt
@@ -1,22 +1,23 @@
 package com.chesire.malime.tests
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.clearText
 import android.support.test.espresso.action.ViewActions.pressImeActionButton
 import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.assertion.ViewAssertions.doesNotExist
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.RootMatchers.withDecorView
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.chesire.malime.INVALID_SEARCH
+import com.chesire.malime.INVALID_SEARCH_NO_ITEMS
 import com.chesire.malime.R
+import com.chesire.malime.tools.ToastMatcher.Companion.onToast
 import com.chesire.malime.view.MainActivity
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotExist
 import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
 import com.schibsted.spain.barista.interaction.BaristaRadioButtonInteractions.clickRadioButtonItem
-import org.hamcrest.Matchers.not
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Rule
@@ -51,9 +52,27 @@ class SearchTests {
     }
 
     @Test
-    @Ignore
-    fun noSearchIsPerformedWithEmptySearchText() {
-        // nothing will come up, so not sure how to check it currently
+    fun noAnimeSearchIsPerformedWithEmptySearchText() {
+        clickRadioButtonItem(R.id.search_option_choices, R.id.search_option_anime_choice)
+        onView(withId(R.id.search_search_term_edit_text)).perform(
+            clearText(),
+            pressImeActionButton()
+        )
+
+        onToast(R.string.search_failed_no_items).check(doesNotExist())
+        onToast(R.string.search_failed_general_error).check(doesNotExist())
+    }
+
+    @Test
+    fun noMangaSearchIsPerformedWithEmptySearchText() {
+        clickRadioButtonItem(R.id.search_option_choices, R.id.search_option_manga_choice)
+        onView(withId(R.id.search_search_term_edit_text)).perform(
+            clearText(),
+            pressImeActionButton()
+        )
+
+        onToast(R.string.search_failed_no_items).check(doesNotExist())
+        onToast(R.string.search_failed_general_error).check(doesNotExist())
     }
 
     @Test
@@ -64,9 +83,7 @@ class SearchTests {
             pressImeActionButton()
         )
 
-        onView(withText(R.string.search_failed_general_error))
-            .inRoot(withDecorView(not(activityRule.activity.window.decorView)))
-            .check(matches(isDisplayed()))
+        onToast(R.string.search_failed_general_error).check(matches(isDisplayed()))
     }
 
     @Test
@@ -77,103 +94,117 @@ class SearchTests {
             pressImeActionButton()
         )
 
-        onView(withText(R.string.search_failed_general_error))
-            .inRoot(withDecorView(not(activityRule.activity.window.decorView)))
-            .check(matches(isDisplayed()))
+        onToast(R.string.search_failed_general_error).check(matches(isDisplayed()))
     }
 
     @Test
     fun failedAnimeSearchNoItemsProducesError() {
         clickRadioButtonItem(R.id.search_option_choices, R.id.search_option_anime_choice)
         onView(withId(R.id.search_search_term_edit_text)).perform(
-            typeText(INVALID_SEARCH),
+            typeText(INVALID_SEARCH_NO_ITEMS),
             pressImeActionButton()
         )
 
-        onView(withText(R.string.search_failed_no_items))
-            .inRoot(withDecorView(not(activityRule.activity.window.decorView)))
-            .check(matches(isDisplayed()))
+        onToast(R.string.search_failed_no_items).check(matches(isDisplayed()))
     }
 
     @Test
     fun failedMangaSearchNoItemsProducesError() {
         clickRadioButtonItem(R.id.search_option_choices, R.id.search_option_manga_choice)
         onView(withId(R.id.search_search_term_edit_text)).perform(
-            typeText(INVALID_SEARCH),
+            typeText(INVALID_SEARCH_NO_ITEMS),
             pressImeActionButton()
         )
 
-        onView(withText(R.string.search_failed_no_items))
-            .inRoot(withDecorView(not(activityRule.activity.window.decorView)))
-            .check(matches(isDisplayed()))
+        onToast(R.string.search_failed_no_items).check(matches(isDisplayed()))
     }
 
     @Test
+    @Ignore
     fun animeSearchIsPerformedWithAnimeSearchOption() {
 
     }
 
     @Test
+    @Ignore
     fun mangaSearchIsPerformedWithAnimeSearchOption() {
 
     }
 
     @Test
+    @Ignore
     fun successfulSearchHasListOfSearchItems() {
 
     }
 
     @Test
+    @Ignore
+    fun successfulSearchCanReturnMultipleTimes() {
+        // return multiple times in onNext with the observer
+    }
+
+    @Test
+    @Ignore
     fun canAddAnimeItemWithCompletedStatus() {
 
     }
 
     @Test
+    @Ignore
     fun canAddAnimeItemWithCurrentStatus() {
 
     }
 
     @Test
+    @Ignore
     fun canAddAnimeItemWithDroppedStatus() {
 
     }
 
     @Test
+    @Ignore
     fun canAddAnimeItemWithOnHoldStatus() {
 
     }
 
     @Test
+    @Ignore
     fun canAddAnimeItemWithPlannedStatus() {
 
     }
 
     @Test
+    @Ignore
     fun canAddMangaItemWithCompletedStatus() {
 
     }
 
     @Test
+    @Ignore
     fun canAddMangaItemWithCurrentStatus() {
 
     }
 
     @Test
+    @Ignore
     fun canAddMangaItemWithDroppedStatus() {
 
     }
 
     @Test
+    @Ignore
     fun canAddMangaItemWithOnHoldStatus() {
 
     }
 
     @Test
+    @Ignore
     fun canAddMangaItemWithPlannedStatus() {
 
     }
 
     @Test
+    @Ignore
     fun itemAlreadyAddedCannotBeAddedAgain() {
 
     }
