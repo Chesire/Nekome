256687b12f6578e7eb79524c0b0632ca624f396c
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-10-27 11:36:34 +0100

    test: add tests for the sharedpref

--- app/src/test/java/com/chesire/malime/util/SharedPrefTests.kt
+++ app/src/test/java/com/chesire/malime/util/SharedPrefTests.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.chesire.malime.R
 import com.chesire.malime.core.flags.SupportedService
+import com.chesire.malime.view.preferences.SortOption
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.mockito.Mockito.RETURNS_SELF
 import org.mockito.Mockito.`when`
@@ -72,5 +74,170 @@ class SharedPrefTests {
 
         classUnderTest.primaryService = SupportedService.MyAnimeList
         verify(mockPrefEditor).putString(PRIMARY_SERVICE, SupportedService.MyAnimeList.name)
+    }
+
+    // TODO add tests for the filter
+    // TODO refactor the filter method after tests
+    fun filterTests() {}
+
+
+    @Test
+    fun `sortOption get() returns SortOption`() {
+        val mockPrefEditor = mock<SharedPreferences.Editor>(defaultAnswer = RETURNS_SELF) { }
+        val mockPreferences = mock<SharedPreferences> {
+            on { edit() }.thenReturn(mockPrefEditor)
+            on {
+                getInt(SORT_OPTION, SortOption.Title.id)
+            }.thenReturn(SortOption.Default.id)
+        }
+        `when`(
+            mockContext.getSharedPreferences(
+                SHARED_PREF_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+        ).thenReturn(mockPreferences)
+
+        val classUnderTest = SharedPref(mockContext)
+
+        assertEquals(SortOption.Default, classUnderTest.sortOption)
+    }
+
+    @Test
+    fun `sortOption set() changes sharedPreferences value`() {
+        val mockPrefEditor = mock<SharedPreferences.Editor>(defaultAnswer = RETURNS_SELF) { }
+        val mockPreferences = mock<SharedPreferences> {
+            on { edit() }.thenReturn(mockPrefEditor)
+        }
+        `when`(
+            mockContext.getSharedPreferences(
+                SHARED_PREF_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+        ).thenReturn(mockPreferences)
+
+        val classUnderTest = SharedPref(mockContext)
+
+        classUnderTest.sortOption = SortOption.StartDate
+        verify(mockPrefEditor).putInt(SORT_OPTION, SortOption.StartDate.id)
+    }
+
+    @Test
+    fun `seriesUpdateSchedulerEnabled get() returns enabled state`() {
+        val mockPrefEditor = mock<SharedPreferences.Editor>(defaultAnswer = RETURNS_SELF) { }
+        val mockPreferences = mock<SharedPreferences> {
+            on { edit() }.thenReturn(mockPrefEditor)
+            on {
+                getBoolean(UPDATE_SCHEDULER_ENABLED, false)
+            }.thenReturn(true)
+        }
+        `when`(
+            mockContext.getSharedPreferences(
+                SHARED_PREF_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+        ).thenReturn(mockPreferences)
+
+        val classUnderTest = SharedPref(mockContext)
+
+        assertTrue(classUnderTest.seriesUpdateSchedulerEnabled)
+    }
+
+    @Test
+    fun `seriesUpdateSchedulerEnabled set() changes sharedPreferences value`() {
+        val mockPrefEditor = mock<SharedPreferences.Editor>(defaultAnswer = RETURNS_SELF) { }
+        val mockPreferences = mock<SharedPreferences> {
+            on { edit() }.thenReturn(mockPrefEditor)
+        }
+        `when`(
+            mockContext.getSharedPreferences(
+                SHARED_PREF_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+        ).thenReturn(mockPreferences)
+
+        val classUnderTest = SharedPref(mockContext)
+
+        classUnderTest.seriesUpdateSchedulerEnabled = false
+        verify(mockPrefEditor).putBoolean(UPDATE_SCHEDULER_ENABLED, false)
+    }
+
+    @Test
+    fun `refreshTokenSchedulerEnabled get() returns enabled state`() {
+        val mockPrefEditor = mock<SharedPreferences.Editor>(defaultAnswer = RETURNS_SELF) { }
+        val mockPreferences = mock<SharedPreferences> {
+            on { edit() }.thenReturn(mockPrefEditor)
+            on {
+                getBoolean(REFRESH_SCHEDULER_ENABLED, false)
+            }.thenReturn(true)
+        }
+        `when`(
+            mockContext.getSharedPreferences(
+                SHARED_PREF_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+        ).thenReturn(mockPreferences)
+
+        val classUnderTest = SharedPref(mockContext)
+
+        assertTrue(classUnderTest.refreshTokenSchedulerEnabled)
+    }
+
+    @Test
+    fun `refreshTokenSchedulerEnabled set() changes sharedPreferences value`() {
+        val mockPrefEditor = mock<SharedPreferences.Editor>(defaultAnswer = RETURNS_SELF) { }
+        val mockPreferences = mock<SharedPreferences> {
+            on { edit() }.thenReturn(mockPrefEditor)
+        }
+        `when`(
+            mockContext.getSharedPreferences(
+                SHARED_PREF_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+        ).thenReturn(mockPreferences)
+
+        val classUnderTest = SharedPref(mockContext)
+
+        classUnderTest.refreshTokenSchedulerEnabled = false
+        verify(mockPrefEditor).putBoolean(REFRESH_SCHEDULER_ENABLED, false)
+    }
+
+    @Test
+    fun `forceBlockServices get() returns if services are blocked`() {
+        val mockPrefEditor = mock<SharedPreferences.Editor>(defaultAnswer = RETURNS_SELF) { }
+        val mockPreferences = mock<SharedPreferences> {
+            on { edit() }.thenReturn(mockPrefEditor)
+            on {
+                getBoolean(FORCE_BLOCK_SERVICES, false)
+            }.thenReturn(true)
+        }
+        `when`(
+            mockContext.getSharedPreferences(
+                SHARED_PREF_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+        ).thenReturn(mockPreferences)
+
+        val classUnderTest = SharedPref(mockContext)
+
+        assertTrue(classUnderTest.forceBlockServices)
+    }
+
+    @Test
+    fun `forceBlockServices set() changes sharedPreferences value`() {
+        val mockPrefEditor = mock<SharedPreferences.Editor>(defaultAnswer = RETURNS_SELF) { }
+        val mockPreferences = mock<SharedPreferences> {
+            on { edit() }.thenReturn(mockPrefEditor)
+        }
+        `when`(
+            mockContext.getSharedPreferences(
+                SHARED_PREF_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+        ).thenReturn(mockPreferences)
+
+        val classUnderTest = SharedPref(mockContext)
+
+        classUnderTest.forceBlockServices = false
+        verify(mockPrefEditor).putBoolean(FORCE_BLOCK_SERVICES, false)
     }
 }
