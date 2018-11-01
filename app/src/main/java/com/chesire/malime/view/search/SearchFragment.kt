117fdf07ede96647eb784b2bfedef51d4a3535be
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-11-01 14:10:25 +0000

    chore: remove full package name being used

--- app/src/main/java/com/chesire/malime/view/search/SearchFragment.kt
+++ app/src/main/java/com/chesire/malime/view/search/SearchFragment.kt
@@ -11,6 +11,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.chesire.malime.R
@@ -62,7 +63,7 @@ class SearchFragment : DaggerFragment(), SearchInteractionListener {
                 setHasFixedSize(true)
                 layoutManager =
                         if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                            androidx.recyclerview.widget.GridLayoutManager(
+                            GridLayoutManager(
                                 requireContext(),
                                 2,
                                 RecyclerView.VERTICAL,
