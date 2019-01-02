660c2d3b098adc6f5eb4f6410b94038f15c17678
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-01-02 20:43:35 +0000

    chore: check returnvalue for schedulers
    Add the `@CheckReturnValue` annotation for returning the schedulers
    for rxjava.

--- app/src/main/java/com/chesire/malime/injection/modules/UiModule.kt
+++ app/src/main/java/com/chesire/malime/injection/modules/UiModule.kt
@@ -7,19 +7,23 @@ import dagger.Module
 import dagger.Provides
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.annotations.CheckReturnValue
 import io.reactivex.schedulers.Schedulers
 
 @Module
 class UiModule {
     @Provides
     @ComputationScheduler
+    @CheckReturnValue
     fun provideComputationScheduler(): Scheduler = Schedulers.computation()
 
     @Provides
     @UIScheduler
+    @CheckReturnValue
     fun provideUiScheduler(): Scheduler = AndroidSchedulers.mainThread()
 
     @Provides
     @IOScheduler
+    @CheckReturnValue
     fun provideIOScheduler(): Scheduler = Schedulers.io()
 }
