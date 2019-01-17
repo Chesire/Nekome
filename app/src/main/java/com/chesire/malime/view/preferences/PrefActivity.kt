d831733759b366670df65e849e4f851834688d50
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-01-17 21:15:10 +0000

    feat: add title to the settings page

--- app/src/main/java/com/chesire/malime/view/preferences/PrefActivity.kt
+++ app/src/main/java/com/chesire/malime/view/preferences/PrefActivity.kt
@@ -8,6 +8,7 @@ class PrefActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        title = getString(R.string.options_settings)
         setContentView(R.layout.activity_pref)
         supportFragmentManager.beginTransaction()
             .replace(R.id.activityPrefFrame, PrefFragment.newInstance(), PrefFragment.tag)
