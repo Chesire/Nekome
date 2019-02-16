1822978d328d1fd9936fe747aa22a4bac0e7f5ad
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-02-16 10:35:09 +0000

    feat: remove some fields to pull for user

--- kitsu/src/main/java/com/chesire/malime/kitsu/api/KitsuUserService.kt
+++ kitsu/src/main/java/com/chesire/malime/kitsu/api/KitsuUserService.kt
@@ -9,6 +9,6 @@ interface KitsuUserService {
     /**
      * Gets the user details, requires authentication to be set.
      */
-    @GET("api/edge/users?filter[self]=true")
+    @GET("api/edge/users?filter[self]=true&fields[users]=id,name,slug,ratingSystem,avatar,coverImage")
     fun getUserDetailsAsync(): Deferred<Response<UserModel>>
 }
