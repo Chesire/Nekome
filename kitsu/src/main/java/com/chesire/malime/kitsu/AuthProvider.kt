aee2c69f53d4f3747b0404cb3320ab44e86f25f0
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-02-20 00:26:01 +0000

    feat: add class to perform encryption/decryption

--- kitsu/src/main/java/com/chesire/malime/kitsu/AuthProvider.kt
+++ kitsu/src/main/java/com/chesire/malime/kitsu/AuthProvider.kt
@@ -1,15 +1,39 @@
 package com.chesire.malime.kitsu
 
 import android.content.SharedPreferences
+import android.util.Base64
 
 private const val ACCESS_TOKEN = "KEY_ACCESS_TOKEN"
 private const val REFRESH_TOKEN = "KEY_REFRESH_TOKEN"
 
-data class AuthProvider(private val preferences: SharedPreferences) {
+class AuthProvider(
+    private val preferences: SharedPreferences,
+    private val cryption: Cryption
+) {
     var accessToken: String
-        get() = preferences.getString(ACCESS_TOKEN, "") ?: ""
-        set(newAccessToken) = preferences.edit().putString(ACCESS_TOKEN, newAccessToken).apply()
+        get() = decryptData(preferences.getString(ACCESS_TOKEN, "") ?: "")
+        set(newAccessToken) {
+            preferences.edit()
+                .putString(ACCESS_TOKEN, encryptData(newAccessToken))
+                .apply()
+        }
+
     var refreshToken: String
-        get() = preferences.getString(REFRESH_TOKEN, "") ?: ""
-        set(newRefreshToken) = preferences.edit().putString(REFRESH_TOKEN, newRefreshToken).apply()
+        get() = decryptData(preferences.getString(REFRESH_TOKEN, "") ?: "")
+        set(newRefreshToken) {
+            preferences.edit()
+                .putString(REFRESH_TOKEN, encryptData(newRefreshToken))
+                .apply()
+        }
+
+    private fun decryptData(data: String): String {
+        return if (data.isEmpty()) {
+            ""
+        } else {
+            cryption.decryptData(Base64.decode(data, Base64.DEFAULT))
+        }
+    }
+
+    private fun encryptData(data: String) =
+        Base64.encodeToString(cryption.encryptText(data), Base64.DEFAULT)
 }
