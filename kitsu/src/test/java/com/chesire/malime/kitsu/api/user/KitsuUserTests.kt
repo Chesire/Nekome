7dbefa9491bc377397cd6fdf2b107472888052f4
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-02-22 22:56:31 +0000

    test: add tests for kitsuuser

---
+++ kitsu/src/test/java/com/chesire/malime/kitsu/api/user/KitsuUserTests.kt
@@ -0,0 +1,121 @@
+package com.chesire.malime.kitsu.api.user
+
+import com.chesire.malime.core.Resource
+import com.chesire.malime.core.models.ImageModel
+import com.chesire.malime.core.models.UserModel
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import okhttp3.ResponseBody
+import org.junit.Assert
+import org.junit.Test
+import retrofit2.Response
+
+class KitsuUserTests {
+    @Test
+    fun `getUserDetails failure response returns Resource#Error with errorBody`() = runBlocking {
+        val expected = "errorBodyString"
+
+        val mockResponseBody = mockk<ResponseBody> {
+            every { string() } returns expected
+        }
+        val mockResponse = mockk<Response<UserModel>> {
+            every { isSuccessful } returns false
+            every { errorBody() } returns mockResponseBody
+        }
+        val mockService = mockk<KitsuUserService> {
+            every {
+                getUserDetailsAsync()
+            } returns async {
+                mockResponse
+            }
+        }
+
+        val classUnderTest = KitsuUser(mockService)
+        val actual = classUnderTest.getUserDetails()
+
+        when (actual) {
+            is Resource.Success -> error("Test has failed")
+            is Resource.Error -> Assert.assertEquals(expected, actual.msg)
+        }
+    }
+
+    @Test
+    fun `getUserDetails failure response returns Resource#Error with message if no error`() =
+        runBlocking {
+            val expected = "responseBodyString"
+
+            val mockResponse = mockk<Response<UserModel>> {
+                every { isSuccessful } returns false
+                every { errorBody() } returns null
+                every { message() } returns expected
+            }
+            val mockService = mockk<KitsuUserService> {
+                every {
+                    getUserDetailsAsync()
+                } returns async {
+                    mockResponse
+                }
+            }
+
+            val classUnderTest = KitsuUser(mockService)
+            val actual = classUnderTest.getUserDetails()
+
+            when (actual) {
+                is Resource.Success -> error("Test has failed")
+                is Resource.Error -> Assert.assertEquals(expected, actual.msg)
+            }
+        }
+
+    @Test
+    fun `getUserDetails successful response with no body returns Resource#Error`() = runBlocking {
+        val expected = "Response body is null"
+
+        val mockResponse = mockk<Response<UserModel>> {
+            every { isSuccessful } returns true
+            every { body() } returns null
+            every { message() } returns expected
+        }
+        val mockService = mockk<KitsuUserService> {
+            every {
+                getUserDetailsAsync()
+            } returns async {
+                mockResponse
+            }
+        }
+
+        val classUnderTest = KitsuUser(mockService)
+        val actual = classUnderTest.getUserDetails()
+
+        when (actual) {
+            is Resource.Success -> error("Test has failed")
+            is Resource.Error -> Assert.assertEquals(expected, actual.msg)
+        }
+    }
+
+    @Test
+    fun `getUserDetails successful response with body returns Resource#Success`() = runBlocking {
+        val expected = UserModel(0, "name", ImageModel.empty, ImageModel.empty)
+
+        val mockResponse = mockk<Response<UserModel>> {
+            every { isSuccessful } returns true
+            every { body() } returns expected
+        }
+        val mockService = mockk<KitsuUserService> {
+            every {
+                getUserDetailsAsync()
+            } returns async {
+                mockResponse
+            }
+        }
+
+        val classUnderTest = KitsuUser(mockService)
+        val actual = classUnderTest.getUserDetails()
+
+        when (actual) {
+            is Resource.Success -> Assert.assertEquals(expected, actual.data)
+            is Resource.Error -> error("Test has failed")
+        }
+    }
+}
