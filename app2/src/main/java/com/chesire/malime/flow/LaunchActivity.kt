c8279517cc288771050b8ef4fbbf65bae791d620
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-02 15:53:12 +0000

    feat: make launch activity load the login temporarily

--- app2/src/main/java/com/chesire/malime/flow/LaunchActivity.kt
+++ app2/src/main/java/com/chesire/malime/flow/LaunchActivity.kt
@@ -1,11 +1,15 @@
 package com.chesire.malime.flow
 
+import android.content.Intent
 import android.os.Bundle
+import com.chesire.malime.flow.login.LoginActivity
 import dagger.android.DaggerActivity
 
 class LaunchActivity : DaggerActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
         // if not performed oob, go to oob flow
 
         // if logged in, go to the logged in flow
