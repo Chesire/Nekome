852f1bbc7218822f71eef2a8a138629a79a5a6fc
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-03 19:02:22 +0000

    test: add tests for the viewmodelfactory

--- app2/src/main/java/com/chesire/malime/flow/ViewModelFactory.kt
+++ app2/src/main/java/com/chesire/malime/flow/ViewModelFactory.kt
@@ -9,13 +9,11 @@ import javax.inject.Provider
 class ViewModelFactory @Inject constructor(
     private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
-
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val creator = creators[modelClass] ?: creators
             .asIterable()
             .firstOrNull { modelClass.isAssignableFrom(it.key) }
-            ?.value
-        ?: throw IllegalArgumentException("unknown model class $modelClass")
+            ?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
 
         return creator.get() as T
     }
