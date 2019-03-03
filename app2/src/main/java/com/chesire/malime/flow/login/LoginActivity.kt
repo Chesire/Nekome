57baef9ab99714b4edede450c5afc6bae103e88b
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-03 18:43:38 +0000

    style: whitespace changes

--- app2/src/main/java/com/chesire/malime/flow/login/LoginActivity.kt
+++ app2/src/main/java/com/chesire/malime/flow/login/LoginActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.chesire.malime.R
 
 class LoginActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
