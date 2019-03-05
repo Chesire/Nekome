757132da49273acf9a648dc65afd4e1f824e1f38
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-05 21:30:55 +0000

    feat: add navigation to each of the fragments

--- app2/src/main/java/com/chesire/malime/flow/overview/OverviewActivity.kt
+++ app2/src/main/java/com/chesire/malime/flow/overview/OverviewActivity.kt
@@ -3,7 +3,14 @@ package com.chesire.malime.flow.overview
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.transaction
 import com.chesire.malime.R
+import com.chesire.malime.flow.overview.activity.ActivityFragment
+import com.chesire.malime.flow.overview.anime.AnimeFragment
+import com.chesire.malime.flow.overview.manga.MangaFragment
+import com.chesire.malime.flow.overview.profile.ProfileFragment
+import com.chesire.malime.flow.overview.settings.SettingsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_overview.activityOverviewNavigation
 
@@ -16,26 +23,35 @@ class OverviewActivity :
         setContentView(R.layout.activity_overview)
 
         activityOverviewNavigation.setOnNavigationItemSelectedListener(this)
+
+        if (savedInstanceState == null) {
+            loadFragment(AnimeFragment::class.java)
+        }
     }
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
         when (p0.itemId) {
-            R.id.overviewNavAnime -> {
-                return true
-            }
-            R.id.overviewNavManga -> {
-                return true
-            }
-            R.id.overviewNavProfile -> {
-                return true
-            }
-            R.id.overviewNavActivity -> {
-                return true
-            }
-            R.id.overviewNavSettings -> {
-                return true
-            }
+            R.id.overviewNavAnime -> loadFragment(AnimeFragment::class.java)
+            R.id.overviewNavManga -> loadFragment(MangaFragment::class.java)
+            R.id.overviewNavProfile -> loadFragment(ProfileFragment::class.java)
+            R.id.overviewNavActivity -> loadFragment(ActivityFragment::class.java)
+            R.id.overviewNavSettings -> loadFragment(SettingsFragment::class.java)
             else -> return false
+        }
+        return true
+    }
+
+    private fun loadFragment(clazz: Class<out Fragment>) {
+        val (fragment, tag) = when (clazz) {
+            AnimeFragment::class.java -> AnimeFragment.newInstance() to AnimeFragment.TAG
+            MangaFragment::class.java -> MangaFragment.newInstance() to MangaFragment.TAG
+            ProfileFragment::class.java -> ProfileFragment.newInstance() to ProfileFragment.TAG
+            ActivityFragment::class.java -> ActivityFragment.newInstance() to ActivityFragment.TAG
+            SettingsFragment::class.java -> SettingsFragment.newInstance() to SettingsFragment.TAG
+            else -> error("Unexpected clazz provided $clazz")
+        }
+        supportFragmentManager.transaction {
+            replace(R.id.activityOverviewContent, fragment, tag)
         }
     }
 }
