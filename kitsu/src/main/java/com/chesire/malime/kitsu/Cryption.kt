ebc3681f4d5c4a3fae88b1f32b791ed0e213588b
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-09 19:45:54 +0000

    refactor: use inner classes for cryption
    While looking at while decryption isn't working in a coroutine this
    was refactored to use some inner classes, since I like how this is
    working left as is.

--- kitsu/src/main/java/com/chesire/malime/kitsu/Cryption.kt
+++ kitsu/src/main/java/com/chesire/malime/kitsu/Cryption.kt
@@ -3,6 +3,7 @@ package com.chesire.malime.kitsu
 import android.annotation.TargetApi
 import android.content.Context
 import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.security.KeyPairGeneratorSpec
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
@@ -24,72 +25,89 @@ private const val ALGORITHM = "RSA"
 private const val ANDROID_KEY_STORE = "AndroidKeyStore"
 private const val ALIAS = "kitsuPrivateAuth"
 
-@Reusable
 class Cryption @Inject constructor(context: Context) {
-    private val keyStore = KeyStore.getInstance(ANDROID_KEY_STORE)
-    private val keySpec: KeyPairGeneratorSpec
+    private val decryption = Decryption()
+    private val encryption = Encryption(context)
 
-    init {
-        keyStore.load(null)
-
-        val startDate = Calendar.getInstance()
-        val endDate = Calendar.getInstance()
-        endDate.add(Calendar.YEAR, 20)
-
-        @Suppress("DEPRECATION")
-        keySpec = KeyPairGeneratorSpec.Builder(context)
-            .setAlias(ALIAS)
-            .setSerialNumber(BigInteger.ONE)
-            .setSubject(X500Principal("CN=$ALIAS CA Certificate"))
-            .setStartDate(startDate.time)
-            .setEndDate(endDate.time)
-            .build()
-    }
-
-    fun encryptText(text: String): ByteArray {
-        val keyPair = createAndroidKeyStoreAsymmetricKey()
-        val cipher = Cipher.getInstance(TRANSFORMATION).apply {
-            init(Cipher.ENCRYPT_MODE, keyPair.public)
-        }
-
-        return cipher.doFinal(text.toByteArray(StandardCharsets.UTF_8))
-    }
-
-    fun decryptData(encryptedData: ByteArray): String {
-        val secretKey = keyStore.getKey(ALIAS, null) as? PrivateKey? ?: return ""
-        val cipher = Cipher.getInstance(TRANSFORMATION).apply {
-            init(Cipher.DECRYPT_MODE, secretKey)
-        }
-
-        return String(cipher.doFinal(encryptedData), StandardCharsets.UTF_8)
-    }
-
+    fun encryptText(text: String) = encryption.encrypt(text)
+    fun decryptData(encryptedData: ByteArray) = decryption.decrypt(encryptedData)
     fun base64Encrypt(input: ByteArray): String = Base64.encodeToString(input, Base64.DEFAULT)
-
     fun base64Decrypt(input: String): ByteArray = Base64.decode(input, Base64.DEFAULT)
 
-    private fun createAndroidKeyStoreAsymmetricKey(): KeyPair {
-        val generator = KeyPairGenerator.getInstance(ALGORITHM, ANDROID_KEY_STORE)
+    private inner class Decryption {
+        private val keyStore = KeyStore.getInstance(ANDROID_KEY_STORE)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            initGeneratorWithKeyGenParameterSpec(generator)
-        } else {
-            generator.initialize(keySpec)
+        init {
+            keyStore.load(null)
         }
 
-        // Generates Key with given spec and saves it to the KeyStore
-        return generator.generateKeyPair()
+        fun decrypt(encryptedData: ByteArray): String {
+            val secretKey = keyStore.getKey(ALIAS, null) as? PrivateKey? ?: return ""
+
+            val cipher = Cipher.getInstance(TRANSFORMATION).apply {
+                init(Cipher.DECRYPT_MODE, secretKey)
+            }
+
+            return String(cipher.doFinal(encryptedData), StandardCharsets.UTF_8)
+        }
     }
 
-    @TargetApi(Build.VERSION_CODES.M)
-    private fun initGeneratorWithKeyGenParameterSpec(generator: KeyPairGenerator) {
-        val builder = KeyGenParameterSpec.Builder(
-            ALIAS,
-            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
-        )
-            .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
-            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+    private inner class Encryption(context: Context) {
+        private val keyStore = KeyStore.getInstance(ANDROID_KEY_STORE)
+        private val keySpec: KeyPairGeneratorSpec
 
-        generator.initialize(builder.build())
+        init {
+            keyStore.load(null)
+
+            val startDate = Calendar.getInstance()
+            val endDate = Calendar.getInstance()
+            endDate.add(Calendar.YEAR, 20)
+
+            @Suppress("DEPRECATION")
+            keySpec = KeyPairGeneratorSpec
+                .Builder(context)
+                .setAlias(ALIAS)
+                .setSerialNumber(BigInteger.ONE)
+                .setSubject(X500Principal("CN=$ALIAS CA Certificate"))
+                .setStartDate(startDate.time)
+                .setEndDate(endDate.time)
+                .build()
+        }
+
+        fun encrypt(text: String): ByteArray {
+            val keyPair = createAndroidKeyStoreAsymmetricKey()
+            val cipher = Cipher.getInstance(TRANSFORMATION).apply {
+                init(Cipher.ENCRYPT_MODE, keyPair.public)
+            }
+
+            return cipher.doFinal(text.toByteArray(StandardCharsets.UTF_8))
+        }
+
+        private fun createAndroidKeyStoreAsymmetricKey(): KeyPair {
+            val generator = KeyPairGenerator.getInstance(ALGORITHM, ANDROID_KEY_STORE)
+
+            if (SDK_INT >= Build.VERSION_CODES.M) {
+                initGeneratorWithKeyGenParameterSpec(generator)
+            } else {
+                initGeneratorWithKeyPairGeneratorSpec(generator)
+            }
+
+            // Generates Key with given spec and saves it to the KeyStore
+            return generator.generateKeyPair()
+        }
+
+        @TargetApi(Build.VERSION_CODES.M)
+        private fun initGeneratorWithKeyGenParameterSpec(generator: KeyPairGenerator) {
+            val builder = KeyGenParameterSpec
+                .Builder(ALIAS, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+
+            generator.initialize(builder.build())
+        }
+
+        private fun initGeneratorWithKeyPairGeneratorSpec(generator: KeyPairGenerator) {
+            generator.initialize(keySpec)
+        }
     }
 }
