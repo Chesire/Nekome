fd14661d3af419246881e3a7fc8c5df0fb84399e
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-09 21:56:30 +0000

    feat: dynamically choose starting location
    Begin adding a dynamic starting location, based on if the user
    has logged in or not.

--- app/src/main/java/com/chesire/malime/flow/LaunchActivity.kt
+++ app/src/main/java/com/chesire/malime/flow/LaunchActivity.kt
@@ -2,18 +2,26 @@ package com.chesire.malime.flow
 
 import android.content.Intent
 import android.os.Bundle
+import com.chesire.malime.flow.login.LoginActivity
 import com.chesire.malime.flow.overview.OverviewActivity
+import com.chesire.malime.kitsu.AuthProvider
 import dagger.android.DaggerActivity
+import javax.inject.Inject
 
 class LaunchActivity : DaggerActivity() {
+    @Inject
+    lateinit var authProvider: AuthProvider
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, OverviewActivity::class.java))
-        finish()
+
         // if not performed oob, go to oob flow
 
-        // if logged in, go to the logged in flow
-
-        // if not logged in go to the login flow
+        val clazz = when {
+            authProvider.accessToken.isEmpty() -> LoginActivity::class.java
+            else -> OverviewActivity::class.java
+        }
+        startActivity(Intent(this, clazz))
+        finish()
     }
 }
