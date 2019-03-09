5f0e6ded397ff40fb32cfd1a7ce33bae9a3679a5
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-09 17:27:26 +0000

    feat: bind the libraryapi

--- app/src/main/java/com/chesire/malime/injection/modules/KitsuModule.kt
+++ app/src/main/java/com/chesire/malime/injection/modules/KitsuModule.kt
@@ -1,7 +1,9 @@
 package com.chesire.malime.injection.modules
 
 import com.chesire.malime.core.api.AuthApi
+import com.chesire.malime.core.api.LibraryApi
 import com.chesire.malime.kitsu.api.auth.KitsuAuth
+import com.chesire.malime.kitsu.api.library.KitsuLibrary
 import dagger.Binds
 import dagger.Module
 
@@ -9,4 +11,7 @@ import dagger.Module
 abstract class KitsuModule {
     @Binds
     abstract fun providesAuthApi(kitsuAuth: KitsuAuth): AuthApi
+
+    @Binds
+    abstract fun providesLibraryApi(kitsuLibrary: KitsuLibrary): LibraryApi
 }
