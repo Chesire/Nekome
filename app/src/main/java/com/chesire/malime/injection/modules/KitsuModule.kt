b4a815ad1b3cdeb75258d4f81578177dfce720d5
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-09 17:28:54 +0000

    feat: bind the userapi

--- app/src/main/java/com/chesire/malime/injection/modules/KitsuModule.kt
+++ app/src/main/java/com/chesire/malime/injection/modules/KitsuModule.kt
@@ -2,11 +2,14 @@ package com.chesire.malime.injection.modules
 
 import com.chesire.malime.core.api.AuthApi
 import com.chesire.malime.core.api.LibraryApi
+import com.chesire.malime.core.api.UserApi
 import com.chesire.malime.kitsu.api.auth.KitsuAuth
 import com.chesire.malime.kitsu.api.library.KitsuLibrary
+import com.chesire.malime.kitsu.api.user.KitsuUser
 import dagger.Binds
 import dagger.Module
 
+@Suppress("unused")
 @Module
 abstract class KitsuModule {
     @Binds
@@ -14,4 +17,7 @@ abstract class KitsuModule {
 
     @Binds
     abstract fun providesLibraryApi(kitsuLibrary: KitsuLibrary): LibraryApi
+
+    @Binds
+    abstract fun providesUserApi(kitsuUser: KitsuUser): UserApi
 }
