79f84af56acd6ff1b48c1725e525950b7f98ebc3
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-05-06 21:49:32 +0100

    feat: update the profile view
    Update profile to have a collapsing toolbar and to show the menu

--- app/src/main/java/com/chesire/malime/flow/profile/ProfileFragment.kt
+++ app/src/main/java/com/chesire/malime/flow/profile/ProfileFragment.kt
@@ -2,16 +2,47 @@ package com.chesire.malime.flow.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.chesire.lifecyklelog.LogLifecykle
+import com.chesire.malime.R
 import com.chesire.malime.databinding.FragmentProfileBinding
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_profile.fragmentProfileToolbar
 
 @LogLifecykle
 class ProfileFragment : DaggerFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = FragmentProfileBinding.inflate(inflater, container, false).root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? AppCompatActivity)?.setSupportActionBar(fragmentProfileToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_profile, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuProfileSettings -> {
+                // show settings screen
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
