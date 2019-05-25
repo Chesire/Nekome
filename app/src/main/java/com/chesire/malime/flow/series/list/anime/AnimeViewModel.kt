97105497cf400b20061b766834e445130ef8f23b
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-05-25 22:44:06 +0100

    fix: update to make the test work
    Instead of using withContext that isn't needed when getting the response, just use the default
    coroutine body

--- app/src/main/java/com/chesire/malime/flow/series/list/anime/AnimeViewModel.kt
+++ app/src/main/java/com/chesire/malime/flow/series/list/anime/AnimeViewModel.kt
@@ -8,9 +8,7 @@ import com.chesire.malime.core.Resource
 import com.chesire.malime.core.flags.UserSeriesStatus
 import com.chesire.malime.core.models.SeriesModel
 import com.chesire.malime.repo.SeriesRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AnimeViewModel @Inject constructor(
@@ -20,14 +18,11 @@ class AnimeViewModel @Inject constructor(
     val animeSeries: LiveData<List<SeriesModel>>
         get() = repo.anime
 
-    fun updateSeries(userSeriesId: Int, newProgress: Int, newUserSeriesStatus: UserSeriesStatus) {
+    fun updateSeries(userSeriesId: Int, newProgress: Int, newUserSeriesStatus: UserSeriesStatus) =
         viewModelScope.launch {
-            val response = withContext(Dispatchers.Default) {
-                repo.updateSeries(userSeriesId, newProgress, newUserSeriesStatus)
-            }
+            val response = repo.updateSeries(userSeriesId, newProgress, newUserSeriesStatus)
             if (response is Resource.Error && response.code == Resource.Error.CouldNotRefresh) {
                 authCaster.issueRefreshingToken()
             }
         }
-    }
 }
