84f7e38f0128c843a8144e876a383f82d46e4ba4
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-06-09 22:34:23 +0100

    chore: add lifecykle logging for settings

--- app/src/main/java/com/chesire/malime/flow/settings/SettingsFragment.kt
+++ app/src/main/java/com/chesire/malime/flow/settings/SettingsFragment.kt
@@ -2,8 +2,10 @@ package com.chesire.malime.flow.settings
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import com.chesire.lifecyklelog.LogLifecykle
 import com.chesire.malime.R
 
+@LogLifecykle
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
