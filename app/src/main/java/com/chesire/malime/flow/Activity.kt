3f3f80b4c806ad22d3566532465ca671223ebeaa
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-07-27 15:00:14 +0100

    refactor: modify activity to use drawerlayout
    Modify so that instead of a bottomnavigation, a drawerlayout is used instead

--- app/src/main/java/com/chesire/malime/flow/Activity.kt
+++ app/src/main/java/com/chesire/malime/flow/Activity.kt
@@ -4,20 +4,21 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
 import androidx.annotation.IdRes
+import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.chesire.lifecyklelog.LogLifecykle
 import com.chesire.malime.AuthCaster
 import com.chesire.malime.LogoutHandler
 import com.chesire.malime.OverviewNavGraphDirections
 import com.chesire.malime.R
 import com.chesire.malime.SharedPref
-import com.chesire.malime.extensions.hide
-import com.chesire.malime.extensions.show
 import com.chesire.malime.kitsu.AuthProvider
+import com.google.android.material.navigation.NavigationView
 import dagger.android.support.DaggerAppCompatActivity
-import kotlinx.android.synthetic.main.activity.activityBottomNavigation
+import kotlinx.android.synthetic.main.activity.activityDrawer
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -35,15 +36,24 @@ class Activity : DaggerAppCompatActivity(), AuthCaster.AuthCasterListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity)
-        loadGraph()
 
+        loadGraph()
         setupNavController()
+
         authCaster.subscribeToAuthError(this)
     }
 
     override fun onDestroy() {
         authCaster.unsubscribeFromAuthError(this)
         super.onDestroy()
+    }
+
+    override fun onBackPressed() {
+        if (activityDrawer.isDrawerOpen(GravityCompat.START)) {
+            activityDrawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     private fun loadGraph() {
@@ -70,16 +80,7 @@ class Activity : DaggerAppCompatActivity(), AuthCaster.AuthCasterListener {
     }
 
     private fun setupNavController() = with(findNavController(R.id.activityNavigation)) {
-        setupWithNavController(activityBottomNavigation, this)
-        addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.animeFragment -> activityBottomNavigation.show()
-                R.id.mangaFragment -> activityBottomNavigation.show()
-                R.id.profileFragment -> activityBottomNavigation.show()
-                R.id.timelineFragment -> activityBottomNavigation.show()
-                else -> activityBottomNavigation.hide()
-            }
-        }
+        findViewById<NavigationView>(R.id.activityNavigationView).setupWithNavController(this)
     }
 
     override fun unableToRefresh() {
@@ -102,4 +103,65 @@ class Activity : DaggerAppCompatActivity(), AuthCaster.AuthCasterListener {
             handlerThread.quitSafely()
         }
     }
+
+
+    /*
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_home -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_tools -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
+    // NavigationView.OnNavigationItemSelectedListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+     */
 }
