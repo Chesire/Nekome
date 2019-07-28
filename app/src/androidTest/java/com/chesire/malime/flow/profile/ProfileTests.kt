56d3c2c27f030359d69716a4cd160dca3bbf02d3
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-07-28 14:07:34 +0100

    test: add test for profile
    Add a test to ensure the profile screen is reached

--- app/src/androidTest/java/com/chesire/malime/flow/profile/ProfileTests.kt
+++ app/src/androidTest/java/com/chesire/malime/flow/profile/ProfileTests.kt
@@ -2,12 +2,17 @@ package com.chesire.malime.flow.profile
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.chesire.malime.R
 import com.chesire.malime.flow.Activity
 import com.chesire.malime.helpers.injector
 import com.chesire.malime.kitsu.AuthProvider
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
+import com.schibsted.spain.barista.interaction.BaristaDrawerInteractions.openDrawer
 import com.schibsted.spain.barista.rule.cleardata.ClearPreferencesRule
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import javax.inject.Inject
 
@@ -25,5 +30,13 @@ class ProfileTests {
     fun setUp() {
         injector.inject(this)
         authProvider.accessToken = "DummyToken"
+    }
+
+    @Test
+    fun canReachProfile() {
+        activity.launchActivity(null)
+        openDrawer()
+        clickOn(R.string.nav_profile)
+        assertDisplayed(R.string.nav_profile)
     }
 }
