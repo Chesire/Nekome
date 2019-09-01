f4521b2799694f7fcf5fb24360ad3981b97e7d86
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-09-01 22:00:59 +0100

    refactor: store the display string for userseriesstatus
    Instead of using an extension method to get the string in the userseriesstatus, just store it in the
    enum

--- core/src/main/java/com/chesire/malime/core/flags/UserSeriesStatus.kt
+++ core/src/main/java/com/chesire/malime/core/flags/UserSeriesStatus.kt
@@ -1,10 +1,13 @@
 package com.chesire.malime.core.flags
 
-enum class UserSeriesStatus(val index: Int) {
-    Unknown(-1),
-    Current(0),
-    Completed(1),
-    OnHold(2),
-    Dropped(3),
-    Planned(4)
+import androidx.annotation.StringRes
+import com.chesire.malime.core.R
+
+enum class UserSeriesStatus(val index: Int, @StringRes val stringId: Int) {
+    Unknown(-1, 0),
+    Current(0, R.string.filter_by_current),
+    Completed(1, R.string.filter_by_completed),
+    OnHold(2, R.string.filter_by_on_hold),
+    Dropped(3, R.string.filter_by_dropped),
+    Planned(4, R.string.filter_by_planned)
 }
