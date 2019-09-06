b3eb4f77acd48bc7c193251ac355c72079fabe15
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-09-06 13:53:52 +0100

    docs: add missing kdoc to the account module

--- account/src/main/java/com/chesire/malime/account/UserRepository.kt
+++ account/src/main/java/com/chesire/malime/account/UserRepository.kt
@@ -7,12 +7,21 @@ import com.chesire.malime.server.Resource
 import com.chesire.malime.server.api.UserApi
 import timber.log.Timber
 
+/**
+ * Repository to interact with user data.
+ */
 class UserRepository(
     private val userDao: UserDao,
     private val userApi: UserApi
 ) {
+    /**
+     * Access the user information.
+     */
     val user = userDao.observe(Service.Kitsu)
 
+    /**
+     * Updates the stored user in the database, data will be funneled to the [user].
+     */
     suspend fun refreshUser(): Resource<UserModel> {
         val response = userApi.getUserDetails()
         when (response) {
@@ -23,5 +32,8 @@ class UserRepository(
         return response
     }
 
+    /**
+     * Retrieves the user id from the database.
+     */
     suspend fun retrieveUserId() = userDao.retrieveUserId(Service.Kitsu)
 }
