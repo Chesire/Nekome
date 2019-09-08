b073ea0935cb9dad675c3ebf7db5db9a664fa23a
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-09-08 10:56:55 +0100

    feat: add interface to aid with controlling the bottom sheet
    Add SheetController, which will allow the detail fragment to control the bottom sheet behavior

--- app/src/main/java/com/chesire/malime/flow/series/list/SeriesListener.kt
+++
@@ -1,8 +0,0 @@
-package com.chesire.malime.flow.series.list
-
-import com.chesire.malime.core.models.SeriesModel
-
-interface SeriesListener {
-    fun loadDetailFragment(seriesModel: SeriesModel)
-    fun loadSearchFragment()
-}
