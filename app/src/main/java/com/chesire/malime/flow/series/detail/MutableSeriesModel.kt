bf807af575e0f2f314480a7f19a21b597ae08da1
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-09-17 22:00:05 +0100

    refactor: add confirm button to the bottom sheet
    Allow confirmation of the changed details in the bottom sheet

--- app/src/main/java/com/chesire/malime/flow/series/detail/MutableSeriesModel.kt
+++ app/src/main/java/com/chesire/malime/flow/series/detail/MutableSeriesModel.kt
@@ -7,6 +7,7 @@ import com.chesire.malime.core.models.SeriesModel
  * Provides an object based on [SeriesModel] that allows modifying values for series detail.
  */
 data class MutableSeriesModel(
+    val userSeriesId: Int,
     val seriesName: String,
     var seriesProgress: Int,
     var seriesLengthValue: Int,
@@ -20,6 +21,7 @@ data class MutableSeriesModel(
          * Creates an instance of [MutableSeriesModel] from an instance of [SeriesModel].
          */
         fun from(model: SeriesModel) = MutableSeriesModel(
+            model.userId,
             model.title,
             model.progress,
             model.totalLength,
