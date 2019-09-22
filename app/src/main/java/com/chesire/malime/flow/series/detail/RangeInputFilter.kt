093cb78e7e3732b5a27bd1b639ed3393c9cbe781
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-09-22 21:29:47 +0100

    style: fix some code style issues
    Remove some redundant code and fix some code style issues

--- app/src/main/java/com/chesire/malime/flow/series/detail/RangeInputFilter.kt
+++ app/src/main/java/com/chesire/malime/flow/series/detail/RangeInputFilter.kt
@@ -2,10 +2,12 @@ package com.chesire.malime.flow.series.detail
 
 import android.text.InputFilter
 import android.text.Spanned
+import timber.log.Timber
 
 /**
  * Provides an [InputFilter] that has a [max] range for the input value.
  */
+@Suppress("ReturnCount", "TooGenericExceptionCaught")
 class RangeInputFilter(private val max: Int) : InputFilter {
     override fun filter(
         source: CharSequence?,
@@ -24,6 +26,7 @@ class RangeInputFilter(private val max: Int) : InputFilter {
             val tempVal = des.substring(0, dstart) + des.substring(dend, des.length)
             tempVal.substring(0, dstart) + src + tempVal.substring(dstart, tempVal.length)
         } catch (ex: IndexOutOfBoundsException) {
+            Timber.w(ex, "Values des: $dest, source: $source")
             null
         }
 
