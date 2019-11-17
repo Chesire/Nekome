0cb8d73a8ae3c98ef09b6c3cd6110ff9329057bb
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-11-17 16:23:55 +0000

    refactor: remove warnings for app-series
    Remove unused imports and add any missing kdoc as required

--- app-series/src/main/java/com/chesire/malime/app/series/list/anime/AnimeFragment.kt
+++ app-series/src/main/java/com/chesire/malime/app/series/list/anime/AnimeFragment.kt
@@ -1,7 +1,5 @@
 package com.chesire.malime.app.series.list.anime
 
-import android.os.Bundle
-import androidx.lifecycle.Observer
 import com.chesire.lifecyklelog.LogLifecykle
 import com.chesire.malime.app.series.list.SeriesListFragment
 import com.chesire.malime.core.flags.SeriesType
