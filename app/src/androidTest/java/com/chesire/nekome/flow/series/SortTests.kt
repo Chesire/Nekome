c3b0a02968704acde42d2f5bacace6394bc88beb
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-01-28 18:38:58 +0000

    test: add tests for the sort dialog
    Test that the dialog has the correct ordering and that pressing on an option closes the dialog

--- app/src/androidTest/java/com/chesire/nekome/flow/series/SortTests.kt
+++ app/src/androidTest/java/com/chesire/nekome/flow/series/SortTests.kt
@@ -8,7 +8,11 @@ import com.chesire.nekome.flow.Activity
 import com.chesire.nekome.helpers.injector
 import com.chesire.nekome.helpers.login
 import com.chesire.nekome.kitsu.AuthProvider
+import com.schibsted.spain.barista.assertion.BaristaListAssertions.assertDisplayedAtPosition
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotExist
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
 import com.schibsted.spain.barista.interaction.BaristaMenuClickInteractions.clickMenu
 import org.junit.Before
 import org.junit.Test
@@ -35,9 +39,20 @@ class SortTests {
         activity.launchActivity(null)
         clickMenu(R.id.menuSeriesListSort)
 
-        assertDisplayed(R.string.sort_by_default)
-        assertDisplayed(R.string.sort_by_end_date)
-        assertDisplayed(R.string.sort_by_start_date)
-        assertDisplayed(R.string.sort_by_title)
+        assertDisplayedAtPosition(R.id.md_recyclerview_content, 0, R.string.sort_by_default)
+        assertDisplayedAtPosition(R.id.md_recyclerview_content, 1, R.string.sort_by_title)
+        assertDisplayedAtPosition(R.id.md_recyclerview_content, 2, R.string.sort_by_start_date)
+        assertDisplayedAtPosition(R.id.md_recyclerview_content, 3, R.string.sort_by_end_date)
+    }
+
+    @Test
+    fun sortDialogClosesOnOptionSelected() {
+        activity.launchActivity(null)
+        clickMenu(R.id.menuSeriesListSort)
+
+        assertDisplayed(R.string.sort_dialog_title)
+        clickOn(R.string.sort_by_title)
+
+        assertNotExist(R.string.sort_dialog_title)
     }
 }
