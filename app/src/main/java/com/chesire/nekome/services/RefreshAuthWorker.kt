97dcd226816a00df3a6d8fd3264462808b286a34
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-02-11 21:01:23 +0000

    fix: add new method to the authapi to refresh auth tokens
    Currently there is no method available to refresh the auth tokens, it is only done in the
    AuthRefreshInterceptor and done through the KitsuService. Add in a new method on the AuthApi to
    allow refreshing auth, no params are passed in as it will handle all of that internally.

--- app/src/main/java/com/chesire/nekome/services/RefreshAuthWorker.kt
+++ app/src/main/java/com/chesire/nekome/services/RefreshAuthWorker.kt
@@ -6,34 +6,38 @@ import androidx.work.WorkerParameters
 import com.chesire.nekome.App
 import com.chesire.nekome.account.UserRepository
 import com.chesire.nekome.server.Resource
+import com.chesire.nekome.server.api.AuthApi
 import timber.log.Timber
 import javax.inject.Inject
 
+
 /**
- * Worker object that handles updating the information for a user.
+ * Worker object that handles updating a users authenticated state.
  *
  * When scheduled to run it will send a request to the [userRepo] to try to refresh the current
- * user data.
+ * user.
  */
-class RefreshUserWorker(
+class RefreshAuthWorker(
     appContext: Context,
     workerParams: WorkerParameters
 ) : CoroutineWorker(appContext, workerParams) {
 
     @Inject
     lateinit var userRepo: UserRepository
+    @Inject
+    lateinit var auth: AuthApi
 
     init {
         // For now setup in the init block
         // dagger currently doesn't support androidInjection for workers
-        Timber.i("Initializing the RefreshUserWorker")
+        Timber.i("Initializing the RefreshAuthWorker")
         if (appContext is App) {
             appContext.daggerComponent.inject(this)
         }
     }
 
     override suspend fun doWork(): Result {
-        Timber.i("doWork RefreshUserWorker")
+        Timber.i("doWork RefreshAuthWorker")
 
         if (userRepo.retrieveUserId() == null) {
             Timber.i("doWork no userId found, so cancelling")
@@ -41,7 +45,7 @@ class RefreshUserWorker(
         }
 
         Timber.i("doWork userId found, beginning to refresh")
-        return if (userRepo.refreshUser() is Resource.Error) {
+        return if (auth.refresh() is Resource.Error) {
             Result.retry()
         } else {
             Result.success()
--- kitsu/src/main/java/com/chesire/nekome/kitsu/api/auth/KitsuAuth.kt
+++ kitsu/src/main/java/com/chesire/nekome/kitsu/api/auth/KitsuAuth.kt
@@ -1,9 +1,9 @@
 package com.chesire.nekome.kitsu.api.auth
 
-import com.chesire.nekome.server.Resource
-import com.chesire.nekome.server.api.AuthApi
 import com.chesire.nekome.kitsu.AuthProvider
 import com.chesire.nekome.kitsu.parse
+import com.chesire.nekome.server.Resource
+import com.chesire.nekome.server.api.AuthApi
 import javax.inject.Inject
 
 /**
@@ -18,16 +18,36 @@ class KitsuAuth @Inject constructor(
         return try {
             when (val response = authService.loginAsync(LoginRequest(username, password)).parse()) {
                 is Resource.Success -> {
-                    authProvider.apply {
-                        accessToken = response.data.accessToken
-                        refreshToken = response.data.refreshToken
-                    }
+                    saveTokens(response.data)
                     Resource.Success(Any())
                 }
                 is Resource.Error -> Resource.Error(response.msg, response.code)
             }
         } catch (ex: Exception) {
             ex.parse()
+        }
+    }
+
+    override suspend fun refresh(): Resource<Any> {
+        return try {
+            when (val response = authService.refreshAccessTokenAsync(
+                RefreshTokenRequest(authProvider.refreshToken)
+            ).parse()) {
+                is Resource.Success -> {
+                    saveTokens(response.data)
+                    Resource.Success(Any())
+                }
+                is Resource.Error -> Resource.Error(response.msg, response.code)
+            }
+        } catch (ex: Exception) {
+            ex.parse()
+        }
+    }
+
+    private fun saveTokens(loginResponse: LoginResponse) {
+        authProvider.apply {
+            accessToken = loginResponse.accessToken
+            refreshToken = loginResponse.refreshToken
         }
     }
 
