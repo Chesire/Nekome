0e2030142566c7d24ef30f1057a5d32a0b891b13
Chesire <troyrijkaard@hotmail.co.uk>
2020-12-12 11:58:37 +0000

    feat: add new entity for storing a series item
    Add a new SeriesEntity that will store a series item in the database. Currently it is using the
    SerieModel, but we want to move away from storing with that

--- libraries/library/src/main/java/com/chesire/nekome/library/LibraryDomainMapper.kt
+++ libraries/library/src/main/java/com/chesire/nekome/library/LibraryDomainMapper.kt
@@ -1,25 +1,64 @@
 package com.chesire.nekome.library
 
 import com.chesire.nekome.core.models.SeriesModel
+import com.chesire.nekome.database.entity.SeriesEntity
 import com.chesire.nekome.library.api.LibraryDomain
 import javax.inject.Inject
 
 /**
- * Provides ability to map instances of [LibraryDomain] into [SeriesModel].
+ * Provides ability to map between object instances for the Library.
  */
 class LibraryDomainMapper @Inject constructor() {
 
     /**
-     * Converts an instance of [LibraryDomain] into a [SeriesModel].
+     * Converts an instance of [LibraryDomain] into a [SeriesEntity].
      */
-    fun toSeriesModel(input: LibraryDomain) =
+    fun toSeriesEntity(input: LibraryDomain) =
+        SeriesEntity(
+            input.id,
+            input.userId,
+            input.type,
+            input.subtype,
+            input.slug,
+            input.title,
+            input.seriesStatus,
+            input.userSeriesStatus,
+            input.progress,
+            input.totalLength,
+            input.posterImage,
+            input.coverImage,
+            input.nsfw,
+            input.startDate,
+            input.endDate
+        )
+
+    fun toSeriesEntity(input: SeriesModel) =
+        SeriesEntity(
+            input.id,
+            input.userId,
+            input.type,
+            input.subtype,
+            input.slug,
+            input.title,
+            input.seriesStatus,
+            input.userSeriesStatus,
+            input.progress,
+            input.totalLength,
+            input.posterImage,
+            input.coverImage,
+            input.nsfw,
+            input.startDate,
+            input.endDate
+        )
+
+    fun toSeriesModel(input: SeriesEntity) =
         SeriesModel(
             input.id,
             input.userId,
             input.type,
             input.subtype,
             input.slug,
-            input.synopsis,
+            "",
             input.title,
             input.seriesStatus,
             input.userSeriesStatus,
