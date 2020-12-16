c834d686034e898807e8bca89a40fbf8880ae8c2
Chesire <troyrijkaard@hotmail.co.uk>
2020-12-16 22:20:51 +0000

    test: fix the ui tests compilation
    UI tests needed to be updated to use the latest models

--- testing/src/main/java/com/chesire/nekome/testing/ObjectCreation.kt
+++ testing/src/main/java/com/chesire/nekome/testing/ObjectCreation.kt
@@ -7,6 +7,7 @@ import com.chesire.nekome.core.flags.Subtype
 import com.chesire.nekome.core.flags.UserSeriesStatus
 import com.chesire.nekome.core.models.ImageModel
 import com.chesire.nekome.core.models.UserModel
+import com.chesire.nekome.database.entity.SeriesEntity
 import com.chesire.nekome.library.SeriesDomain
 
 /**
@@ -28,6 +29,40 @@ fun createSeriesDomain(
     startDate: String = "",
     endDate: String = ""
 ) = SeriesDomain(
+    id,
+    userId,
+    seriesType,
+    subType,
+    slug,
+    title,
+    seriesStatus,
+    userSeriesStatus,
+    progress,
+    totalLength,
+    posterImage,
+    startDate,
+    endDate
+)
+
+/**
+ * Creates a new instance of [SeriesEntity] with default data.
+ */
+@Suppress("LongParameterList", "LongMethod")
+fun createSeriesEntity(
+    id: Int = 999,
+    userId: Int = 999,
+    seriesType: SeriesType = SeriesType.Anime,
+    subType: Subtype = Subtype.TV,
+    slug: String = "slug",
+    title: String = "title",
+    seriesStatus: SeriesStatus = SeriesStatus.Current,
+    userSeriesStatus: UserSeriesStatus = UserSeriesStatus.Current,
+    progress: Int = 0,
+    totalLength: Int = 0,
+    posterImage: ImageModel = ImageModel.empty,
+    startDate: String = "",
+    endDate: String = ""
+) = SeriesEntity(
     id,
     userId,
     seriesType,
