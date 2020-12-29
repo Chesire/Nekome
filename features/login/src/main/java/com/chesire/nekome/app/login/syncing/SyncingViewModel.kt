301de8990e6eb8b76bcc07180e85221544541932
Chesire <troyrijkaard@hotmail.co.uk>
2020-12-29 21:15:23 +0000

    refactor: remove converting to live data twice in SyncingViewModel
    Instead of calling the Transformations class on a flow converted to live data, then executing some
    operations on it. Execute operations on the flow itself, then finally convert into a livedata to
    pass to the application layer

--- features/login/src/main/java/com/chesire/nekome/app/login/syncing/SyncingViewModel.kt
+++ features/login/src/main/java/com/chesire/nekome/app/login/syncing/SyncingViewModel.kt
@@ -1,8 +1,8 @@
 package com.chesire.nekome.app.login.syncing
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
@@ -13,6 +13,7 @@ import com.chesire.nekome.core.extensions.postError
 import com.chesire.nekome.core.extensions.postSuccess
 import com.chesire.nekome.core.flags.AsyncState
 import com.chesire.nekome.library.SeriesRepository
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 /**
@@ -24,14 +25,15 @@ class SyncingViewModel @ViewModelInject constructor(
 ) : ViewModel() {
 
     private val _syncStatus = MutableLiveData<AsyncState<Any, Any>>()
-    val syncStatus = _syncStatus
-    val avatarUrl = Transformations.map(userRepo.user.asLiveData()) { user ->
-        if (user is User.Found) {
-            user.domain.avatar.largest?.url
-        } else {
-            ""
-        }
-    }
+    val syncStatus: LiveData<AsyncState<Any, Any>> get() = _syncStatus
+    val avatarUrl = userRepo.user
+        .map { user ->
+            if (user is User.Found) {
+                user.domain.avatar.largest?.url
+            } else {
+                ""
+            }
+        }.asLiveData()
 
     /**
      * Sets off the process for pulling down and storing the users series.
