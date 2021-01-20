e8a16e7bc58c3f8822e79e3c6a99d820aa0fb6d2
Chesire <troyrijkaard@hotmail.co.uk>
2021-01-20 22:47:26 +0000

    test: add the SearchRobot and a test to hit the search screen
    Add a SearchRobot that will allow navigating over the search screen. Also add a test to ensure the
    Search screen can be reached

---
+++ app/src/androidTest/java/com/chesire/nekome/features/search/SearchTests.kt
@@ -0,0 +1,47 @@
+package com.chesire.nekome.features.search
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.chesire.nekome.helpers.launchActivity
+import com.chesire.nekome.helpers.login
+import com.chesire.nekome.injection.DatabaseModule
+import com.chesire.nekome.kitsu.AuthProvider
+import com.chesire.nekome.robots.activity
+import com.chesire.nekome.robots.search.search
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+@HiltAndroidTest
+@UninstallModules(DatabaseModule::class)
+@RunWith(AndroidJUnit4::class)
+class SearchTests {
+
+    @get:Rule
+    val hilt = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var authProvider: AuthProvider
+
+    @Before
+    fun setUp() {
+        hilt.inject()
+        authProvider.login()
+    }
+
+    @Test
+    fun canReachSearch() {
+        launchActivity()
+
+        activity {
+            goToSearch()
+        }
+        search {
+            validate { isVisible() }
+        }
+    }
+}
--- app/src/androidTest/java/com/chesire/nekome/robots/search/SearchRobot.kt
+++ app/src/androidTest/java/com/chesire/nekome/robots/search/SearchRobot.kt
@@ -1,4 +1,57 @@
 package com.chesire.nekome.robots.search
 
+import com.chesire.nekome.R
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
+import com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo
+
+/**
+ * Method to interact with the [SearchRobot].
+ */
+fun search(func: SearchRobot.() -> Unit) = SearchRobot().apply { func() }
+
+/**
+ * Robot to interact with the search screen.
+ */
 class SearchRobot {
+
+    /**
+     * Enters the term to search for.
+     */
+    fun searchTerm(term: String) = writeTo(R.id.searchText, term)
+
+    /**
+     * Selects the anime chip.
+     */
+    fun selectAnime() = clickOn(R.id.searchChipAnime)
+
+    /**
+     * Selects the manga chip.
+     */
+    fun selectManga() = clickOn(R.id.searchChipManga)
+
+    /**
+     * Clicks the search button.
+     */
+    fun clickSearch() = clickOn(R.id.searchConfirmButton)
+
+    /**
+     * Executes validation steps.
+     */
+    infix fun validate(func: SearchResultRobot.() -> Unit): SearchResultRobot {
+        return SearchResultRobot().apply { func() }
+    }
+}
+
+/**
+ * Robot to check the results for the search screen.
+ */
+class SearchResultRobot {
+
+    /**
+     * Asserts the search screen is shown.
+     */
+    fun isVisible() {
+        assertDisplayed(R.id.searchLayout)
+    }
 }
