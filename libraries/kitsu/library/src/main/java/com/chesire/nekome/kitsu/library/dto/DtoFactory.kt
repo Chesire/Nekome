3aa77839c3c94862cee2eb6bf6cbae20fe2b9ebd
Chesire <troyrijkaard@hotmail.co.uk>
2021-02-20 21:37:56 +0000

    refactor: remove the rating for kitsu if the value is too low
    If the value is less than 2 then do not add the value for the rating to the json that gets sent up
    to the api

--- libraries/kitsu/library/src/main/java/com/chesire/nekome/kitsu/library/dto/DtoFactory.kt
+++ libraries/kitsu/library/src/main/java/com/chesire/nekome/kitsu/library/dto/DtoFactory.kt
@@ -1,5 +1,6 @@
 package com.chesire.nekome.kitsu.library.dto
 
+import org.json.JSONObject
 import javax.inject.Inject
 
 /**
@@ -50,18 +51,19 @@ class DtoFactory @Inject constructor() {
         newProgress: Int,
         newStatus: String,
         rating: Int
-    ) =
-        """
-{
-  "data": {
-    "id": $userSeriesId,
-    "type": "libraryEntries",
-    "attributes": {
-      "progress": $newProgress,
-      "status": "$newStatus",
-      "ratingTwenty": $rating
+    ): String {
+        val attributesObject = JSONObject()
+            .apply {
+                put("progress", newProgress)
+                put("status", newStatus)
+                if (rating > 1) put("ratingTwenty", rating)
+            }
+        val dataObject = JSONObject()
+            .apply {
+                put("id", userSeriesId)
+                put("type", "libraryEntries")
+                put("attributes", attributesObject)
+            }
+        return JSONObject().put("data", dataObject).toString()
     }
-  }
-}
-        """.trimIndent()
 }
