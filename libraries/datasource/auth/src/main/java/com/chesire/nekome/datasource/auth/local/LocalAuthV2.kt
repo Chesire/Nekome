b6c86e49932dca6f49a6737f5e4c5b2de546edb8
Chesire <troyrijkaard@hotmail.co.uk>
2021-09-10 22:27:59 +0100

    refactor: add encrypted shared preferences for higher sdk versions
    Higher Android SDK versions can use the new androidx crypto library, add using this encrypted
    preferences for these Android versions and fallback to unencrypted preferences when we are on a
    lower sdk version.

--- libraries/datasource/auth/src/main/java/com/chesire/nekome/datasource/auth/local/LocalAuthV2.kt
+++ libraries/datasource/auth/src/main/java/com/chesire/nekome/datasource/auth/local/LocalAuthV2.kt
@@ -1,15 +1,54 @@
 package com.chesire.nekome.datasource.auth.local
 
+import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
 import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import dagger.Reusable
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
+private const val ENCRYPTED_NEKOME_AUTH_PREF = "ENekomePrefStore"
+private const val UNENCRYPTED_NEKOME_AUTH_PREF = "UneNekomePrefStore"
 private const val ACCESS_TOKEN = "KEY_KITSU_ACCESS_TOKEN_V2"
 private const val REFRESH_TOKEN = "KEY_KITSU_REFRESH_TOKEN_v2"
 
+@Reusable
 class LocalAuthV2 @Inject constructor(
-    private val preferences: SharedPreferences
+    @ApplicationContext private val context: Context
 ) : LocalAuth {
+
+    private val preferences: SharedPreferences
+
+    init {
+        val unencryptedPreferences = initializeUnencryptedPreferences()
+        preferences = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            initializeEncryptedPreferences().also {
+                unencryptedPreferences.migrateToEncrypted(it)
+            }
+        } else {
+            unencryptedPreferences
+        }
+    }
+
+    private fun SharedPreferences.migrateToEncrypted(encryptedPreferences: SharedPreferences) {
+        if (all.isEmpty()) {
+            return
+        }
+        all.forEach { (key, value) ->
+            if (value is String) {
+                encryptedPreferences.edit {
+                    putString(key, value)
+                }
+            }
+        }
+        edit {
+            remove(ACCESS_TOKEN)
+            remove(REFRESH_TOKEN)
+        }
+    }
 
     override val hasCredentials: Boolean
         get() = preferences.contains(ACCESS_TOKEN) || preferences.contains(REFRESH_TOKEN)
@@ -26,12 +65,27 @@ class LocalAuthV2 @Inject constructor(
         get() = preferences.getString(REFRESH_TOKEN, "") ?: ""
         set(newRefreshToken) {
             preferences.edit {
-                putString(ACCESS_TOKEN, newRefreshToken)
+                putString(REFRESH_TOKEN, newRefreshToken)
             }
         }
 
     override fun clear() = preferences.edit {
         remove(ACCESS_TOKEN)
         remove(REFRESH_TOKEN)
+    }
+
+    private fun initializeEncryptedPreferences(): SharedPreferences {
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        return EncryptedSharedPreferences.create(
+            ENCRYPTED_NEKOME_AUTH_PREF,
+            masterKeyAlias,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    private fun initializeUnencryptedPreferences(): SharedPreferences {
+        return context.getSharedPreferences(UNENCRYPTED_NEKOME_AUTH_PREF, Context.MODE_PRIVATE)
     }
 }
