ccfde158705de7f61871a8ef7573cd88c263dbed
Chesire <troy.rijkaard@chesire.dev>
2022-12-22 11:44:31 +0000

    feat: add systemuicontroller for compose

--- core/compose/src/main/java/com/chesire/core/compose/theme/Theme.kt
+++ core/compose/src/main/java/com/chesire/core/compose/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = LightBlue219
@@ -18,10 +20,16 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun NekomeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val systemUiController = rememberSystemUiController()
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+    systemUiController.apply {
+        setSystemBarsColor(color = Color.Transparent)
+        setNavigationBarColor(color = Color.Transparent)
+        setStatusBarColor(color = Color.Transparent)
     }
 
     MaterialTheme(
