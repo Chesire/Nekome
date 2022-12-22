98c4bd4b7c428af646a0aa3ddf6b81fae1fa0779
Chesire <troy.rijkaard@chesire.dev>
2022-12-22 15:54:36 +0000

    feat: add populate user details to credentials screen

---
+++ features/login/src/main/java/com/chesire/nekome/app/login/credentials/core/PopulateUserDetailsUseCase.kt
@@ -0,0 +1,24 @@
+package com.chesire.nekome.app.login.credentials.core
+
+import com.chesire.nekome.core.Resource
+import com.chesire.nekome.datasource.user.UserRepository
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
+import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class PopulateUserDetailsUseCase @Inject constructor(private val user: UserRepository) {
+
+    suspend operator fun invoke(): Result<Unit, Unit> {
+        return withContext(Dispatchers.IO) {
+            val result = user.refreshUser()
+            if (result is Resource.Success) {
+                Ok(Unit)
+            } else {
+                Err(Unit)
+            }
+        }
+    }
+}
--- features/login/src/main/java/com/chesire/nekome/app/login/credentials/ui/CredentialsViewModel.kt
+++ features/login/src/main/java/com/chesire/nekome/app/login/credentials/ui/CredentialsViewModel.kt
@@ -3,6 +3,8 @@ package com.chesire.nekome.app.login.credentials.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.chesire.nekome.app.login.R
+import com.chesire.nekome.app.login.credentials.core.ClearCredentialsUseCase
+import com.chesire.nekome.app.login.credentials.core.PopulateUserDetailsUseCase
 import com.chesire.nekome.app.login.credentials.core.VerifyCredentialsFailure
 import com.chesire.nekome.app.login.credentials.core.VerifyCredentialsUseCase
 import com.github.michaelbull.result.onFailure
@@ -10,78 +12,84 @@ import com.github.michaelbull.result.onSuccess
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class CredentialsViewModel @Inject constructor(
-    private val verifyCredentials: VerifyCredentialsUseCase
+    private val verifyCredentials: VerifyCredentialsUseCase,
+    private val populateUserDetails: PopulateUserDetailsUseCase,
+    private val clearCredentials: ClearCredentialsUseCase
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UIState.empty)
-    val uiState: StateFlow<UIState> = _uiState
+    val uiState = _uiState.asStateFlow()
+    private var state: UIState
+        get() = _uiState.value
+        set(value) {
+            _uiState.update { value }
+        }
 
     fun execute(viewAction: ViewAction) {
         when (viewAction) {
-            is ViewAction.UsernameChanged -> _uiState.update { viewState ->
-                viewState.copy(
-                    username = viewAction.newUsername,
-                    usernameError = false,
-                    buttonEnabled = viewAction.newUsername.isNotBlank() && viewState.password.isNotBlank()
-                )
-            }
-            is ViewAction.PasswordChanged -> _uiState.update { viewState ->
-                viewState.copy(
-                    password = viewAction.newPassword,
-                    passwordError = false,
-                    buttonEnabled = viewState.username.isNotBlank() && viewAction.newPassword.isNotBlank()
-                )
-            }
+            is ViewAction.UsernameChanged -> state = state.copy(
+                username = viewAction.newUsername,
+                usernameError = false,
+                buttonEnabled = viewAction.newUsername.isNotBlank() && state.password.isNotBlank()
+            )
+            is ViewAction.PasswordChanged -> state = state.copy(
+                password = viewAction.newPassword,
+                passwordError = false,
+                buttonEnabled = state.username.isNotBlank() && viewAction.newPassword.isNotBlank()
+            )
             ViewAction.LoginPressed -> performLogin()
-            ViewAction.ErrorSnackbarObserved -> _uiState.update { viewState ->
-                viewState.copy(errorSnackbar = ErrorSnackbar(false, 0))
+            ViewAction.ErrorSnackbarObserved -> {
+                state = state.copy(errorSnackbar = ErrorSnackbar(false, 0))
             }
         }
     }
 
     private fun performLogin() = viewModelScope.launch {
-        _uiState.update { viewState -> viewState.copy(isPerformingLogin = true) }
+        state = state.copy(isPerformingLogin = true)
 
-        val state = uiState.value
         verifyCredentials(username = state.username, password = state.password)
-            .onSuccess {
-                // TODO: Finish this screen, make new screen for user details and series?
-            }
-            .onFailure(::handleCredentialsFailure)
+            .onSuccess { loadUserDetails() }
+            .onFailure(::handleLoginFailure)
     }
 
-    private fun handleCredentialsFailure(failure: VerifyCredentialsFailure) {
-        when (failure) {
-            VerifyCredentialsFailure.InvalidCredentials -> _uiState.update { viewState ->
-                viewState.copy(
-                    isPerformingLogin = false,
-                    errorSnackbar = ErrorSnackbar(true, R.string.login_error_credentials)
-                )
-            }
-            VerifyCredentialsFailure.NetworkError -> _uiState.update { viewState ->
-                viewState.copy(
-                    isPerformingLogin = false,
-                    errorSnackbar = ErrorSnackbar(true, R.string.login_error_generic)
-                )
-            }
-            VerifyCredentialsFailure.PasswordInvalid -> _uiState.update { viewState ->
-                viewState.copy(
-                    passwordError = true,
+    private suspend fun loadUserDetails() {
+        populateUserDetails()
+            .onSuccess {
+                state = state.copy(
                     isPerformingLogin = false
                 )
             }
-            VerifyCredentialsFailure.UsernameInvalid -> _uiState.update { viewState ->
-                viewState.copy(
-                    usernameError = true,
-                    isPerformingLogin = false
-                )
+            .onFailure {
+                clearCredentials()
+                handleLoginFailure(VerifyCredentialsFailure.NetworkError)
             }
+    }
+
+    private fun handleLoginFailure(failure: VerifyCredentialsFailure) {
+        val newState = when (failure) {
+            VerifyCredentialsFailure.InvalidCredentials -> state.copy(
+                isPerformingLogin = false,
+                errorSnackbar = ErrorSnackbar(true, R.string.login_error_credentials)
+            )
+            VerifyCredentialsFailure.NetworkError -> state.copy(
+                isPerformingLogin = false,
+                errorSnackbar = ErrorSnackbar(true, R.string.login_error_generic)
+            )
+            VerifyCredentialsFailure.PasswordInvalid -> state.copy(
+                passwordError = true,
+                isPerformingLogin = false
+            )
+            VerifyCredentialsFailure.UsernameInvalid -> state.copy(
+                usernameError = true,
+                isPerformingLogin = false
+            )
         }
+        state = newState
     }
 }
