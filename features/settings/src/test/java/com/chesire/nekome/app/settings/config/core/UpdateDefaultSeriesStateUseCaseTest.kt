ebf01eb23325062e8b36f020f7e79b98d76be5cd
Chesire <troy.rijkaard@chesire.dev>
2023-03-30 16:27:00 +0100

    test: add unit tests for the settings module

---
+++ features/settings/src/test/java/com/chesire/nekome/app/settings/config/core/UpdateDefaultSeriesStateUseCaseTest.kt
@@ -0,0 +1,37 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.chesire.nekome.app.settings.config.core
+
+import com.chesire.nekome.core.flags.UserSeriesStatus
+import com.chesire.nekome.core.preferences.ApplicationPreferences
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class UpdateDefaultSeriesStateUseCaseTest {
+
+    private val pref = mockk<ApplicationPreferences>()
+    private lateinit var updateDefaultSeriesState: UpdateDefaultSeriesStateUseCase
+
+    @Before
+    fun setup() {
+        clearAllMocks()
+
+        updateDefaultSeriesState = UpdateDefaultSeriesStateUseCase(pref)
+    }
+
+    @Test
+    fun `When invoking, update pref with new value`() = runTest {
+        coEvery { pref.updateDefaultHomeScreen(any()) } just runs
+
+        pref.updateDefaultSeriesState(UserSeriesStatus.OnHold)
+
+        coEvery { pref.updateDefaultSeriesState(UserSeriesStatus.OnHold) }
+    }
+}
---
+++ features/settings/src/test/java/com/chesire/nekome/app/settings/config/core/UpdateRateSeriesUseCaseTest.kt
@@ -0,0 +1,36 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.chesire.nekome.app.settings.config.core
+
+import com.chesire.nekome.core.preferences.SeriesPreferences
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class UpdateRateSeriesUseCaseTest {
+
+    private val pref = mockk<SeriesPreferences>()
+    private lateinit var updateRateSeries: UpdateRateSeriesUseCase
+
+    @Before
+    fun setup() {
+        clearAllMocks()
+
+        updateRateSeries = UpdateRateSeriesUseCase(pref)
+    }
+
+    @Test
+    fun `When invoking, update pref with new value`() = runTest {
+        coEvery { pref.updateRateSeriesOnCompletion(any()) } just runs
+
+        pref.updateRateSeriesOnCompletion(true)
+
+        coEvery { pref.updateRateSeriesOnCompletion(true) }
+    }
+}
