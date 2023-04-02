4b70ccb24d57ce9b14d7fbe25bbbb0d82e574697
Chesire <troy.rijkaard@chesire.dev>
2023-04-02 11:08:38 +0100

    feat: load anime or fragment screen based on arg

--- app/src/main/java/com/chesire/nekome/MainActivity.kt
+++ app/src/main/java/com/chesire/nekome/MainActivity.kt
@@ -11,15 +11,18 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.chesire.lifecyklelog.LogLifecykle
 import com.chesire.nekome.app.login.credentials.ui.CredentialsScreen
 import com.chesire.nekome.app.search.host.ui.HostScreen
 import com.chesire.nekome.app.series.collection.ui.CollectionScreen
 import com.chesire.nekome.app.settings.config.ui.ConfigScreen
 import com.chesire.nekome.core.compose.theme.NekomeTheme
+import com.chesire.nekome.core.flags.SeriesType
 import dagger.hilt.android.AndroidEntryPoint
 
 @LogLifecykle
@@ -55,22 +58,37 @@ class MainActivity : ComponentActivity() {
                         ) {
                             NavHost(
                                 navController = navController,
-                                startDestination = "login"
+                                startDestination = "anime"
                             ) {
                                 composable("login") {
                                     CredentialsScreen {
-                                        // TODO: Finish probably can be handled better now
+                                        // TODO: Implement the navigation to the sub screen here?
+                                        // TODO: Probably better inside the module
                                     }
                                 }
-                                composable("anime") {
-                                    // TODO: Pass in the anime/manga flag here too
+                                composable(
+                                    route = "anime",
+                                    arguments = listOf(
+                                        navArgument("seriesType") {
+                                            type = NavType.ParcelableType(SeriesType::class.java)
+                                            defaultValue = SeriesType.Anime
+                                        }
+                                    )
+                                ) {
                                     CollectionScreen { seriesId, seriesTitle ->
                                         // TODO: Implement the navigation to the sub screen here?
                                         // TODO: Probably better inside the module
                                     }
                                 }
-                                composable("manga") {
-                                    // TODO: Pass in the anime/manga flag here too
+                                composable(
+                                    route = "manga",
+                                    arguments = listOf(
+                                        navArgument("seriesType") {
+                                            type = NavType.ParcelableType(SeriesType::class.java)
+                                            defaultValue = SeriesType.Manga
+                                        }
+                                    )
+                                ) {
                                     CollectionScreen { seriesId, seriesTitle ->
                                         // TODO: Implement the navigation to the sub screen here?
                                         // TODO: Probably better inside the module
