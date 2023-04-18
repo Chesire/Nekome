ab9aac2021e5d360986562c2641680485dcf41f4
Chesire <troy.rijkaard@chesire.dev>
2023-04-18 20:22:44 +0100

    fix: make use of the user setting for theme
    Add user settings for dynamic themes, and make use of them when loading the app.

--- app/src/main/java/com/chesire/nekome/ui/MainActivity.kt
+++ app/src/main/java/com/chesire/nekome/ui/MainActivity.kt
@@ -3,21 +3,50 @@ package com.chesire.nekome.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.collectAsState
 import com.chesire.lifecyklelog.LogLifecykle
 import com.chesire.nekome.core.compose.theme.NekomeTheme
+import com.chesire.nekome.core.preferences.ApplicationPreferences
+import com.chesire.nekome.core.preferences.flags.Theme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @LogLifecykle
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @Inject
+    lateinit var appSettings: ApplicationPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            NekomeTheme {
+            val themeState = appSettings.theme.collectAsState(initial = Theme.System)
+            val (isDarkMode, isDynamicColor) = generateTheme(
+                theme = themeState.value,
+                isSystemDarkMode = isSystemInDarkTheme()
+            )
+            NekomeTheme(
+                isDarkTheme = isDarkMode,
+                isDynamicColor = isDynamicColor
+            ) {
                 MainActivityScreen()
             }
+        }
+    }
+
+    /**
+     * Returns a [Pair] of (dark mode enabled) to (dynamic color enabled).
+     */
+    private fun generateTheme(theme: Theme, isSystemDarkMode: Boolean): Pair<Boolean, Boolean> {
+        return when (theme) {
+            Theme.System -> isSystemDarkMode to true
+            Theme.Dark -> true to false
+            Theme.Light -> false to false
+            Theme.DynamicDark -> true to true
+            Theme.DynamicLight -> false to true
         }
     }
 }
