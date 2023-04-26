152c590fd8c2e45c37dc80079422c433145e5b29
Chesire <troy.rijkaard@chesire.dev>
2023-04-26 23:51:58 +0100

    refactor: remove the gson dependency
    Remove the gson dependency and add an abomination to god to parse the model into json and vice
    versa.

--- libraries/database/src/main/java/com/chesire/nekome/database/converters/LinkModelsListConverter.kt
+++ libraries/database/src/main/java/com/chesire/nekome/database/converters/LinkModelsListConverter.kt
@@ -2,8 +2,11 @@ package com.chesire.nekome.database.converters
 
 import androidx.room.TypeConverter
 import com.chesire.nekome.core.models.LinkModel
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+import com.chesire.nekome.core.models.LinkModelJsonAdapter
+import com.squareup.moshi.Moshi
+import java.io.IOException
+
+private const val SEPARATOR = ",,SEP,,"
 
 /**
  * Converter for list of [LinkModel] -> [String].
@@ -12,16 +15,37 @@ import com.google.gson.reflect.TypeToken
  */
 class LinkModelsListConverter {
 
+    // I'm sorry future me.
+
+    private val adapter: LinkModelJsonAdapter by lazy {
+        LinkModelJsonAdapter(Moshi.Builder().build())
+    }
+
     /**
      * Converts a list of [LinkModel] into a [String].
      */
     @TypeConverter
-    fun fromListOfLinkModels(links: List<LinkModel>): String = Gson().toJson(links)
+    fun fromListOfLinkModels(links: List<LinkModel>): String {
+        return links.joinToString(separator = SEPARATOR) { adapter.toJson(it) }
+    }
 
     /**
      * Converts a [String] into a list of [LinkModel].
      */
     @TypeConverter
-    fun toListOfLinkModels(linksJson: String): List<LinkModel> =
-        Gson().fromJson(linksJson, object : TypeToken<List<LinkModel?>?>() {}.type) ?: emptyList()
+    fun toListOfLinkModels(linksJson: String): List<LinkModel> {
+        return if (linksJson.isBlank()) {
+            emptyList()
+        } else {
+            linksJson
+                .split(SEPARATOR)
+                .mapNotNull {
+                    try {
+                        adapter.fromJson(it)
+                    } catch (ex: IOException) {
+                        null
+                    }
+                }
+        }
+    }
 }
