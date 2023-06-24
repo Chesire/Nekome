417c817c3655911f0e538df3e57f488f76b2048b
Chesire <troy.rijkaard@chesire.dev>
2023-06-24 18:09:57 +0100

    test: fix search object creation in tests

--- libraries/kitsu/search/src/test/java/com/chesire/nekome/kitsu/search/SearchCreation.kt
+++ libraries/kitsu/search/src/test/java/com/chesire/nekome/kitsu/search/SearchCreation.kt
@@ -14,6 +14,7 @@ fun createSearchItemDto(type: SeriesType) =
         type,
         SearchItemDto.Attributes(
             "synopsis",
+            mapOf("en" to "en"),
             "canonicalTitle",
             Subtype.Unknown,
             ImageModel.empty
