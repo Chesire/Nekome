8a98d3d9cb16e5c8c128e1cf2fe11b45470fd470
Chesire <troy.rijkaard@chesire.dev>
2023-06-24 14:00:54 +0100

    feat: choose the correct title to use on series lists

--- core/preferences/src/main/java/com/chesire/nekome/core/preferences/flags/TitleLanguage.kt
+++ core/preferences/src/main/java/com/chesire/nekome/core/preferences/flags/TitleLanguage.kt
@@ -6,11 +6,11 @@ import com.chesire.nekome.core.preferences.R
 /**
  * Options available for the language used to display the titles.
  */
-enum class TitleLanguage(val index: Int, @StringRes val stringId: Int) {
-    Canonical(0, R.string.title_language_canonical),
-    English(1, R.string.title_language_english),
-    Romaji(2, R.string.title_language_romaji),
-    Japanese(3, R.string.title_language_japanese);
+enum class TitleLanguage(val index: Int, @StringRes val stringId: Int, val key: String) {
+    Canonical(0, R.string.title_language_canonical, ""),
+    English(1, R.string.title_language_english, "en"),
+    Romaji(2, R.string.title_language_romaji, "en_jp"),
+    Japanese(3, R.string.title_language_japanese, "ja_jp");
 
     companion object {
 
