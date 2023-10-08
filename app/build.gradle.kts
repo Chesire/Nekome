7daec9dbcf3ed0d86a5a19a6129fc80da22294e5
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2023-10-08 17:59:08 +0100

    chore: update version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 21
         targetSdk = libs.versions.sdk.get().toInt()
         versionCode = 23100818 // Date of build formatted as 'yyMMddHH'
-        versionName = "2.0.5"
+        versionName = "2.1.0"
         testInstrumentationRunner = "com.chesire.nekome.TestRunner"
         resourceConfigurations += listOf("en", "ja")
     }
