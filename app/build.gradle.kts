19a91e5ff8deed8e6c7d833b2714e98a773f7f01
Troy Rijkaard <Chesire@users.noreply.github.com>
2024-01-20 09:49:19 +0000

    chore: update version and changelog (#1160)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.chesire.nekome"
         minSdk = 21
         targetSdk = libs.versions.sdk.get().toInt()
-        versionCode = 24010710 // Date of build formatted as 'yyMMddHH'
-        versionName = "2.2.0"
+        versionCode = 24012009 // Date of build formatted as 'yyMMddHH'
+        versionName = "2.2.1"
         testInstrumentationRunner = "com.chesire.nekome.TestRunner"
         resourceConfigurations += listOf("en", "ja")
     }
