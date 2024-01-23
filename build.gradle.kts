93993057233cdb5673e4fac17d96659e3e1763c7
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2024-01-23 17:35:30 +0000

    chore: remove unusued navigation args plugin
    This plugin hasn't been used since moving to compose navigation, and wasn't required anymore.
    
    Closes #1163

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     alias(libs.plugins.aboutlibraries) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.androidx.navigation.safeargs) apply false
     alias(libs.plugins.google.dagger.hilt.android) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.kotlin.android) apply false
